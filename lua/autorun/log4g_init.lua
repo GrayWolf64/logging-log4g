78c21daab71c835d1875caba40f7c05f59034681
GrayWolf <88234821+GrayWolf64@users.noreply.github.com>
2022-12-03 23:04:53 +0800

    Delete lua/autorun directory

--- lua/autorun/log4g_init.lua
+++
@@ -1,9 +0,0 @@
-log4g = log4g or {}
-file.CreateDir("log4g")
-
-if CLIENT then
-    file.CreateDir("log4g/client")
-else
-    file.CreateDir("log4g/server")
-    file.CreateDir("log4g/server/loggercontext")
-end
