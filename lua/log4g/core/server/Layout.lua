b5e4db9679b7bbe1cfb58bdcb2480bd06075dff0
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-04 14:37:11 +0800

    Log4g.Instances._Layouts.

--- lua/log4g/core/server/Layout.lua
+++ lua/log4g/core/server/Layout.lua
@@ -1,5 +1,6 @@
 --- The Layout.
 -- @classmod Layout
+Log4g.Instances._Layouts = Log4g.Instances._Layouts or {}
 local Layout = include("log4g/core/impl/Class.lua"):Extend()
 
 function Layout:New(name, func)
@@ -14,10 +15,10 @@ end
 -- @return object layout
 function Log4g.Core.Layouts.RegisterLayout(name, func)
     local layout = Layout(name, func)
-    table.insert(Log4g.Core.Layouts, layout)
+    table.insert(Log4g.Instances._Layouts, layout)
 
     return layout
 end
 
 local PatternLayout = include("log4g/core/server/layout/PatternLayout.lua")
-Log4g.Core.Layouts.PatternLayout = Layout("PatternLayout", PatternLayout)
+Log4g.Instances._Layouts.PatternLayout = Layout("PatternLayout", PatternLayout)
