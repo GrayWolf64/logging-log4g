4748de9ba5767c6bea4fb5db4233fed713c34096
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-05 09:09:16 +0800

    Removed LifeCycle tbl.

--- lua/log4g/core/server/LifeCycle.lua
+++ lua/log4g/core/server/LifeCycle.lua
@@ -1,10 +1,1 @@
 Log4g.Core.LifeCycle = Log4g.Core.LifeCycle or {}
-
-Log4g.Core.LifeCycle._States = {
-    INITIALIZING = 1,
-    INITIALIZED = 2,
-    STARTING = 3,
-    STARTED = 4,
-    STOPPING = 5,
-    STOPPED = 6
-}
