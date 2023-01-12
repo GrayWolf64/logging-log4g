d5645e403372b1a6c8805554f1d77c3d37714012
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-12 20:02:41 +0800

    A place for Logger instances.

--- lua/log4g/core/server/Logger.lua
+++ lua/log4g/core/server/Logger.lua
@@ -1,6 +1,7 @@
 --- The Logger.
 -- @classmod Logger
 Log4g.Logger = Log4g.Logger or {}
+Log4g.Inst._Loggers = Log4g.Inst._Loggers or {}
 local Logger = include("log4g/core/impl/Class.lua"):Extend()
 
 function Logger:New(name, func, loggerconfig, layout)
