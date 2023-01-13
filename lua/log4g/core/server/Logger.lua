7ad71453c68c2ec742ba548caa90f60a1dc6c711
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-13 09:18:23 +0800

    Fix type.

--- lua/log4g/core/server/Logger.lua
+++ lua/log4g/core/server/Logger.lua
@@ -7,5 +7,5 @@ local Logger = include("log4g/core/impl/Class.lua"):Extend()
 function Logger:New(name, func, loggerconfig, layout)
     self.name = name or ""
     self.func = func or ""
-    self.loggerconfig = loggerconfig or ""
+    self.loggerconfig = loggerconfig or {}
 end
