09309f2f5bb89505c80243ed21cd92db8d33608d
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-14 09:03:42 +0800

    Removed another boilerplate, use -- @license instead.

--- lua/log4g/core/server/config/ClientGUIConfigurator.lua
+++ lua/log4g/core/server/config/ClientGUIConfigurator.lua
@@ -1,20 +1,6 @@
---[[
-    Copyright 2023 GrayWolf64
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
---]]
 --- Server-side processing of the Client GUI requests.
 -- @script ClientGUIConfigurator.lua
+-- @license Apache License 2.0
 local AddNetworkStrsViaTbl = Log4g.Util.AddNetworkStrsViaTbl
 local FindFilesInSubFolders = Log4g.Util.FindFilesInSubFolders
 local SendTableAfterRcvNetMsg = Log4g.Util.SendTableAfterRcvNetMsg
