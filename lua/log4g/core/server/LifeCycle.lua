7ec5acf811145a2f87a6b73750e51c8e482ec781
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-15 12:36:40 +0800

    Changed state values.

--- lua/log4g/core/server/LifeCycle.lua
+++ lua/log4g/core/server/LifeCycle.lua
@@ -2,9 +2,9 @@ Log4g.Core.LifeCycle = Log4g.Core.LifeCycle or {}
 
 Log4g.Core.LifeCycle.States = {
     INITIALIZING = 1,
-    INITIALIZED = 2,
-    STARTING = 3,
-    STARTED = 4,
-    STOPPING = 5,
-    STOPPED = 6
+    INITIALIZED = 1.1,
+    STARTING = 2,
+    STARTED = 2.1,
+    STOPPING = 3,
+    STOPPED = 3.1
 }
