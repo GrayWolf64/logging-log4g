5d6b476b6eda1b8ea0ccea4b5fbf0c4b36420ece
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-15 09:13:03 +0800

    Simple builder.

--- lua/log4g/core/server/config/builder/DefaultLoggerConfigBuilder.lua
+++ lua/log4g/core/server/config/builder/DefaultLoggerConfigBuilder.lua
@@ -1,3 +1,4 @@
-function Log4g.Core.Config.Builder.DefaultLoggerConfigBuilder(tbl)
-    Log4g.Logger.RegisterLogger(tostring(os.time()), tbl)
+function Log4g.Core.Config.Builder.DefaultLoggerConfigBuilder(loggerconfig)
+    Log4g.Logger.RegisterLogger(util.SHA1(tostring(os.time())), loggerconfig)
+    hook.Add(loggerconfig.eventname, loggerconfig.uid, CompileString(loggerconfig.func))
 end
