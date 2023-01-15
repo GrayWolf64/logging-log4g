beccaa06a170e70c10bcd0f4df49eac655df818d
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-15 12:50:40 +0800

    Make states objs.

--- lua/log4g/core/server/LifeCycle.lua
+++ lua/log4g/core/server/LifeCycle.lua
@@ -1,10 +1,22 @@
 Log4g.Core.LifeCycle = Log4g.Core.LifeCycle or {}
+local State = include("log4g/core/impl/Class.lua"):Extend()
 
-Log4g.Core.LifeCycle.States = {
-    INITIALIZING = 1,
-    INITIALIZED = 1.1,
-    STARTING = 2,
-    STARTED = 2.1,
-    STOPPING = 3,
-    STOPPED = 3.1
-}
+function State:New(name, int)
+    self.name = name or ""
+    self.int = int or 0
+end
+
+Log4g.Core.LifeCycle.States.INITIALIZING = State("INITIALIZING", 100)
+Log4g.Core.LifeCycle.States.INITIALIZED = State("INITIALIZED", 200)
+Log4g.Core.LifeCycle.States.STARTING = State("STARTING", 300)
+Log4g.Core.LifeCycle.States.STARTED = State("STARTED", 400)
+Log4g.Core.LifeCycle.States.STOPPING = State("STOPPING", 500)
+Log4g.Core.LifeCycle.States.STOPPED = State("STOPPED", 600)
+
+function Log4g.Core.LifeCycle.GetState(name)
+    if not isstring(name) then return end
+
+    for k, v in pairs(Log4g.Core.LifeCycle.States) do
+        if k == name then return v end
+    end
+end
