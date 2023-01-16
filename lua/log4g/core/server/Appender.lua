685a4d0ffd74dab315731d2d5e913a1723c6e6c1
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-16 22:08:24 +0800

    Fixed Appender.

--- lua/log4g/core/server/Appender.lua
+++ lua/log4g/core/server/Appender.lua
@@ -2,19 +2,20 @@
 -- @classmod Appender
 Log4g.Core.Appender = Log4g.Core.Appender or {}
 Log4g.Inst._Appenders = Log4g.Inst._Appenders or {}
-local Appender = include("log4g/core/impl/Class.lua"):Extend()
+local Class = include("log4g/core/impl/MiddleClass.lua")
+local Appender = Class("Appender")
 
-function Appender:New(name, func)
+function Appender:Initialize(name, func)
     self.name = name or ""
     self.func = func or function() end
 end
 
+local AppendConsole = include("log4g/core/server/appender/ConsoleAppender.lua")
+Log4g.Inst._Appenders.ConsoleAppender = Appender:New("ConsoleAppender", AppendConsole)
+
 function Log4g.Core.Appender.RegisterAppender(name, func)
-    local appender = Appender(name, func)
+    local appender = Appender:New(name, func)
     table.insert(Log4g.Inst._Appenders, appender)
 
     return appender
 end
-
-local AppendConsole = include("log4g/core/server/appender/ConsoleAppender.lua")
-Log4g.Inst._Appenders.ConsoleAppender = Appender("ConsoleAppender", AppendConsole)
