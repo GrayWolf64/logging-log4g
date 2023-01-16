d869fe6b604c4c8217a914311b72ff386058ad19
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-16 21:08:55 +0800

    Fixed LifeCycle.

--- lua/log4g/core/server/LifeCycle.lua
+++ lua/log4g/core/server/LifeCycle.lua
@@ -1,18 +1,19 @@
 Log4g.Core.LifeCycle = Log4g.Core.LifeCycle or {}
 Log4g.Core.LifeCycle._States = Log4g.Core.LifeCycle._States or {}
-local State = include("log4g/core/impl/Class.lua"):Extend()
+local Class = include("log4g/core/impl/MiddleClass.lua")
+local State = Class("State")
 
-function State:New(name, int)
+function State:Initialize(name, int)
     self.name = name or ""
     self.int = int or 0
 end
 
-Log4g.Core.LifeCycle._States.INITIALIZING = State("INITIALIZING", 100)
-Log4g.Core.LifeCycle._States.INITIALIZED = State("INITIALIZED", 200)
-Log4g.Core.LifeCycle._States.STARTING = State("STARTING", 300)
-Log4g.Core.LifeCycle._States.STARTED = State("STARTED", 400)
-Log4g.Core.LifeCycle._States.STOPPING = State("STOPPING", 500)
-Log4g.Core.LifeCycle._States.STOPPED = State("STOPPED", 600)
+Log4g.Core.LifeCycle._States.INITIALIZING = State:New("INITIALIZING", 100)
+Log4g.Core.LifeCycle._States.INITIALIZED = State:New("INITIALIZED", 200)
+Log4g.Core.LifeCycle._States.STARTING = State:New("STARTING", 300)
+Log4g.Core.LifeCycle._States.STARTED = State:New("STARTED", 400)
+Log4g.Core.LifeCycle._States.STOPPING = State:New("STOPPING", 500)
+Log4g.Core.LifeCycle._States.STOPPED = State:New("STOPPED", 600)
 
 function Log4g.Core.LifeCycle.GetState(name)
     if not isstring(name) then return end
