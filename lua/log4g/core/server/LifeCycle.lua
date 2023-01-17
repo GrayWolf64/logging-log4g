4337960b7a4ee9b666b17ee69498a3047614b27e
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-17 09:16:52 +0800

    Formatted LifeCycle.

--- lua/log4g/core/server/LifeCycle.lua
+++ lua/log4g/core/server/LifeCycle.lua
@@ -1,4 +1,3 @@
 Log4g.Core.LifeCycle = Log4g.Core.LifeCycle or {}
-Log4g.Core.LifeCycle._States = Log4g.Core.LifeCycle._States or {}
 
 Log4g.Core.LifeCycle._States = {"INITIALIZING", "INITIALIZED", "STARTING", "STARTED", "STOPPING", "STOPPED"}
