5225bef002e514c15e4b387f31c31321d39a2d52
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-17 09:15:51 +0800

    Removed useless state related content.

--- lua/log4g/core/server/LifeCycle.lua
+++ lua/log4g/core/server/LifeCycle.lua
@@ -1,24 +1,4 @@
 Log4g.Core.LifeCycle = Log4g.Core.LifeCycle or {}
 Log4g.Core.LifeCycle._States = Log4g.Core.LifeCycle._States or {}
-local Class = include("log4g/core/impl/MiddleClass.lua")
-local State = Class("State")
 
-function State:Initialize(name, int)
-    self.name = name or ""
-    self.int = int or 0
-end
-
-Log4g.Core.LifeCycle._States.INITIALIZING = State:New("INITIALIZING", 100)
-Log4g.Core.LifeCycle._States.INITIALIZED = State:New("INITIALIZED", 200)
-Log4g.Core.LifeCycle._States.STARTING = State:New("STARTING", 300)
-Log4g.Core.LifeCycle._States.STARTED = State:New("STARTED", 400)
-Log4g.Core.LifeCycle._States.STOPPING = State:New("STOPPING", 500)
-Log4g.Core.LifeCycle._States.STOPPED = State:New("STOPPED", 600)
-
-function Log4g.Core.LifeCycle.GetState(name)
-    if not isstring(name) then return end
-
-    for k, v in pairs(Log4g.Core.LifeCycle.States) do
-        if k == name then return v end
-    end
-end
+Log4g.Core.LifeCycle._States = {"INITIALIZING", "INITIALIZED", "STARTING", "STARTED", "STOPPING", "STOPPED"}
