de32f43786a91fbf5aa4cdd49746196a33e8b2ab
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-17 09:07:19 +0800

    Removed sha256 Version.

--- lua/log4g/core/server/Version.lua
+++ lua/log4g/core/server/Version.lua
@@ -2,9 +2,7 @@
 -- @script Version.lua
 Log4g.___Version = Log4g.___Version or {}
 Log4g.___Version.LastCommit = "3a53f34448fb4afa1425c04f3dad1de78fb0bb8b"
-Log4g.___Version.SHA256 = util.SHA256(Log4g.___Version.LastCommit)
 
 concommand.Add("Log4g_Version", function()
     MsgC("Log4g Version (Last Commit): " .. Log4g.___Version.LastCommit .. "\n")
-    MsgC("SHA256: " .. Log4g.___Version.SHA256 .. "\n")
 end)
