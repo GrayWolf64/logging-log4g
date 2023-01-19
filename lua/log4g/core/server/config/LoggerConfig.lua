5c02eedf7fe49170775b21bf75aa4a9212c50630
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-19 11:48:45 +0800

    Added STARTED for LoggerConfig.

--- lua/log4g/core/server/config/LoggerConfig.lua
+++ lua/log4g/core/server/config/LoggerConfig.lua
@@ -5,7 +5,9 @@ Log4g.Core.Config.Builder = Log4g.Core.Config.Builder or {}
 Log4g.Core.Config.LoggerConfig = Log4g.Core.Config.LoggerConfig or {}
 Log4g.Core.Config.LoggerConfig.Buffer = Log4g.Core.Config.LoggerConfig.Buffer or {}
 local Class = include("log4g/core/impl/MiddleClass.lua")
+local Stateful = include("log4g/core/impl/Stateful.lua")
 local LoggerConfig = Class("LoggerConfig")
+LoggerConfig:include(Stateful)
 local HasKey = Log4g.Util.HasKey
 
 function LoggerConfig:Initialize(tbl)
@@ -35,8 +37,14 @@ function LoggerConfig:Remove()
         Log4g.Core.Config.LoggerConfig.Buffer[self.name] = nil
         MsgN("LoggerConfig deletion: Successfully removed LoggerConfig from Buffer.")
     else
-        ErrorNoHalt("LoggerConfig deletion failed: Can't find the LoggerContext in Buffer, may be removed already.\n")
+        ErrorNoHalt("LoggerConfig deletion failed: Can't find the LoggerConfig in Buffer, may be removed already.\n")
     end
+end
+
+local STARTED = LoggerConfig:AddState("STARTED")
+
+function STARTED:Remove()
+    ErrorNoHalt("LoggerConfig deletion failed: The LoggerConfig is already started and applied to a Logger.\n")
 end
 
 function Log4g.Core.Config.LoggerConfig.RegisterLoggerConfig(tbl)
