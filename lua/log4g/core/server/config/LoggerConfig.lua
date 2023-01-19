f92d9220c481e1f70911e1a89ec0d173905c70e4
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-19 09:17:24 +0800

    Fixed LoggerConfig args.

--- lua/log4g/core/server/config/LoggerConfig.lua
+++ lua/log4g/core/server/config/LoggerConfig.lua
@@ -16,7 +16,7 @@ function LoggerConfig:Initialize(tbl)
     self.level = tbl.level
     self.appender = tbl.appender
     self.layout = tbl.layout
-    self.file = "log4g/server/loggercontext/" .. loggercontext .. "/loggerconfig/" .. name .. ".json"
+    self.file = "log4g/server/loggercontext/" .. tbl.loggercontext .. "/loggerconfig/" .. tbl.name .. ".json"
     self.func = tbl.func
 end
 
@@ -29,7 +29,7 @@ function Log4g.Core.Config.LoggerConfig.RegisterLoggerConfig(tbl)
     if not HasKey(Log4g.Core.Config.LoggerConfig.Buffer, tbl.name) then
         local loggerconfig = LoggerConfig:New(tbl)
         Log4g.Core.Config.LoggerConfig.Buffer[tbl.name] = loggerconfig
-        file.Write("log4g/server/loggercontext/" .. tbl.loggercontext .. "/loggerconfig/" .. tbl.name .. ".json", util.TableToJSON(tbl, true))
+        file.Write(loggerconfig.file, util.TableToJSON(tbl, true))
 
         return loggerconfig
     else
