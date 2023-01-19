12ede8005ef5c5e1f932bf464c431e47649e6373
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-19 12:37:34 +0800

    Removed basic version chk.

--- lua/log4g/core/server/Version.lua
+++ lua/log4g/core/server/Version.lua
@@ -1,8 +1,4 @@
 --- Version handler.
 -- @script Version.lua
 Log4g._Version = Log4g._Version or {}
-Log4g._Version.LastCommit = "3a53f34448fb4afa1425c04f3dad1de78fb0bb8b"
-
-concommand.Add("Log4g_Version", function()
-    MsgC("Log4g Version (Last Commit): " .. Log4g._Version.LastCommit .. "\n")
-end)
+concommand.Add("Log4g_Version", function() end)
