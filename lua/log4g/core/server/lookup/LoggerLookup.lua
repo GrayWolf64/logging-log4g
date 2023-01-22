72c60b8157d16c0582d6a666858c882540cee3ec
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-22 12:04:31 +0800

    Added comments for AddItem of LoggerLookup.

--- lua/log4g/core/server/lookup/LoggerLookup.lua
+++ lua/log4g/core/server/lookup/LoggerLookup.lua
@@ -4,6 +4,10 @@ Log4g.Logger.Lookup = Log4g.Logger.Lookup or {}
 local File = "log4g/server/loggercontext/lookup_logger.json"
 
 --- Add a table item to Logger Lookup whose key is Logger name and its content are the associated names of LoggerContext and LoggerConfig.
+-- If the Lookup doesn't exist, it will create one and write into it.
+-- @param loggername The name of the Logger to write
+-- @param contextname The name of the LoggerContext to write
+-- @param configfile The string filepath of the Logger's associated LoggerConfig to write
 function Log4g.Logger.Lookup.AddItem(loggername, contextname, configfile)
     if not file.Exists(File, "DATA") then
         file.Write(File, util.TableToJSON({
@@ -22,4 +26,7 @@ function Log4g.Logger.Lookup.AddItem(loggername, contextname, configfile)
 
         file.Write(File, util.TableToJSON(tbl, true))
     end
+end
+
+function Log4g.Logger.Lookup.RemoveLoggerViaContext(contextname)
 end
