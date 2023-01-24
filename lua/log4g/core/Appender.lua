91939e2b0a09bc26e5c3394088154c0200875979
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-24 13:37:29 +0800

    Changed structure.

--- lua/log4g/core/Appender.lua
+++ lua/log4g/core/Appender.lua
@@ -1,7 +1,6 @@
 --- The Appender.
 -- @classmod Appender
 Log4g.Core.Appender = Log4g.Core.Appender or {}
-Log4g.Core.Appender.Buffer = Log4g.Core.Appender.Buffer or {}
 local Class = include("log4g/core/impl/MiddleClass.lua")
 local Appender = Class("Appender")
 
@@ -12,9 +11,9 @@ end
 
 function Log4g.Core.Appender.RegisterAppender(name, func)
     local appender = Appender:New(name, func)
-    table.insert(Log4g.Core.Appender.Buffer, appender)
+    table.insert(Log4g.Core.Appender, appender)
 
     return appender
 end
 
-Log4g.Core.Appender.Buffer.ConsoleAppender = Appender:New("ConsoleAppender", include("log4g/core/appender/ConsoleAppender.lua"))
+Log4g.Core.Appender.ConsoleAppender = Appender:New("ConsoleAppender", include("log4g/core/appender/ConsoleAppender.lua"))
