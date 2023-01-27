962faa835ff27158ea9b81f98893dc3083cff10f
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-27 23:24:18 +0800

    Updated comments.

--- lua/log4g/mmc-gui/MMC-Init.lua
+++ lua/log4g/mmc-gui/MMC-Init.lua
@@ -1,6 +1,6 @@
 --- Initialization of Log4g MMC GUI on server and client.
 -- It's based on [Derma](https://wiki.facepunch.com/gmod/Derma_Basic_Guide) UI system that comes with the game itself.
--- @script Log4g-MMC-Init.lua
+-- @script MMC-Init.lua
 if SERVER then
     include("log4g/mmc-gui/server/ClientGUIConfigurator.lua")
     include("log4g/mmc-gui/server/ClientGUIManagement.lua")
