66e6c965decb1891078c4f9cb1ddd17a596a7879
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-31 08:36:54 +0800

    Changed LoggerContextLookup.

--- lua/log4g/core/lookup/LoggerContextLookup.lua
+++ lua/log4g/core/lookup/LoggerContextLookup.lua
@@ -21,24 +21,24 @@ function Log4g.Core.LoggerContext.Lookup.AddContext(name)
     end
 end
 
---- Add a string item to LoggerContext Lookup using the names of a LoggerContext and LoggerConfig.
+--- Add a LoggerConfig name item to LoggerContext Lookup depending on its LoggerContext name.
 -- If the LoggerContext Lookup file doesn't exist, a new file will be created and data will be written into.
 -- If the file exists, new data will be written into while keeping the previous data.
--- @param contextname The LoggerContext name to write
--- @param configname The LoggerConfig name to write
-function Log4g.Core.LoggerContext.Lookup.AddItem(contextname, configname)
+-- @param context The LoggerContext name to put the LoggerConfig in
+-- @param config The LoggerConfig name to write
+function Log4g.Core.LoggerContext.Lookup.AddConfig(context, config)
     if not file.Exists(File, "DATA") then
         file.Write(File, util.TableToJSON({
-            [contextname] = {configname}
+            [context] = {config}
         }, true))
     else
         local tbl = util.JSONToTable(file.Read(File, "DATA"))
-        local bool, key = HasKey(tbl, contextname)
+        local bool, key = HasKey(tbl, context)
 
         if bool then
-            table.insert(tbl[key], configname)
+            table.insert(tbl[key], config)
         else
-            tbl[contextname] = {configname}
+            tbl[context] = {config}
         end
 
         file.Write(File, util.TableToJSON(tbl, true))
@@ -61,15 +61,15 @@ function Log4g.Core.LoggerContext.Lookup.RemoveContext(name)
 end
 
 --- Remove a LoggerConfig name item from the LoggerContext Lookup.
--- @param contextname The name of the LoggerContext that the LoggerConfig is in
--- @param configname The name of the LoggerConfig to find and remove from the Lookup table
-function Log4g.Core.LoggerContext.Lookup.RemoveLoggerConfig(contextname, configname)
+-- @param context The name of the LoggerContext that the LoggerConfig is in
+-- @param config The name of the LoggerConfig to find and remove from the Lookup table
+function Log4g.Core.LoggerContext.Lookup.RemoveConfig(context, config)
     local tbl = util.JSONToTable(file.Read(File, "DATA"))
 
     for k, v in pairs(tbl) do
-        if k == contextname then
+        if k == context then
             for i, j in ipairs(v) do
-                if j == configname then
+                if j == config then
                     table.remove(v, i)
                 end
             end
