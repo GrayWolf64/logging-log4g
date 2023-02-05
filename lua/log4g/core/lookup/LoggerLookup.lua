b552b8e53990e5d4eeab9daf0a11f4dcccbcad4c
graywolf64 <tairikuookami@foxmail.com>
2023-02-05 16:13:59 +0800

    SQL LoggerLookup.

--- lua/log4g/core/lookup/LoggerLookup.lua
+++ lua/log4g/core/lookup/LoggerLookup.lua
@@ -6,41 +6,44 @@
 local File = "log4g/server/loggercontext/lookup_logger.json"
 local LoggerLookup = Log4g.Core.Logger.Lookup
 
+local function UpdateLookup(tbl)
+	sql.Query(
+		"UPDATE Log4g_Lookup SET Content = " .. sql.SQLStr(util.TableToJSON(tbl, true)) .. " WHERE Name = 'Logger';"
+	)
+end
+
 --- Add a table item to Logger Lookup whose key is Logger name and its content are the associated names of LoggerContext and file paths of LoggerConfig.
 -- If the Lookup doesn't exist, it will create one and write into it.
 -- @param loggername The name of the Logger to write
 -- @param contextname The name of the LoggerContext to write
 -- @param configfile The string filepath of the Logger's associated LoggerConfig to write
 function LoggerLookup.AddItem(loggername, contextname, configfile)
-	if not file.Exists(File, "DATA") then
-		file.Write(
-			File,
-			util.TableToJSON({
-				[loggername] = {
-					loggercontext = contextname,
-					configfile = configfile,
-				},
-			}, true)
-		)
+	if not sql.QueryRow("SELECT * FROM Log4g_Lookup WHERE Name = 'Logger';") then
+		sql.Query("INSERT INTO Log4g_Lookup (Name, Content) VALUES('Logger', " .. sql.SQLStr(util.TableToJSON({
+			[loggername] = {
+				loggercontext = contextname,
+				configfile = configfile,
+			},
+		}, true)) .. ")")
 	else
-		local tbl = util.JSONToTable(file.Read(File, "DATA"))
+		local tbl = util.JSONToTable(sql.QueryValue("SELECT Content FROM Log4g_Lookup WHERE Name = 'Logger';"))
 
 		tbl[loggername] = {
 			loggercontext = contextname,
 			configfile = configfile,
 		}
 
-		file.Write(File, util.TableToJSON(tbl, true))
+		UpdateLookup(tbl)
 	end
 end
 
 --- Remove any Logger items whose LoggerContext's name is the given contextname.
 -- @param contextname The LoggerContext's name
 function LoggerLookup.RemoveLoggerViaContext(contextname)
-	if not file.Exists(File, "DATA") then
+	if not sql.QueryRow("SELECT * FROM Log4g_Lookup WHERE Name = 'Logger';") then
 		return
 	end
-	local tbl = util.JSONToTable(file.Read(File, "DATA"))
+	local tbl = util.JSONToTable(sql.QueryValue("SELECT Content FROM Log4g_Lookup WHERE Name = 'Logger';"))
 	if table.IsEmpty(tbl) then
 		return
 	end
@@ -51,17 +54,17 @@ function LoggerLookup.RemoveLoggerViaContext(contextname)
 		end
 	end
 
-	file.Write(File, util.TableToJSON(tbl, true))
+	UpdateLookup(tbl)
 end
 
 --- Remove a Logger item from LoggerLookup.
 -- @param contextname The LoggerContext's name
 -- @param loggername The Logger's name
 function LoggerLookup.RemoveLogger(contextname, loggername)
-	if not file.Exists(File, "DATA") then
+	if not sql.QueryRow("SELECT * FROM Log4g_Lookup WHERE Name = 'Logger';") then
 		return
 	end
-	local tbl = util.JSONToTable(file.Read(File, "DATA"))
+	local tbl = util.JSONToTable(sql.QueryValue("SELECT Content FROM Log4g_Lookup WHERE Name = 'Logger';"))
 	if table.IsEmpty(tbl) then
 		return
 	end
@@ -72,5 +75,5 @@ function LoggerLookup.RemoveLogger(contextname, loggername)
 		end
 	end
 
-	file.Write(File, util.TableToJSON(tbl, true))
+	UpdateLookup(tbl)
 end
