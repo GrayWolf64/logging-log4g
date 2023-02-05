416aaa31e8b4a09a15f3f0b19f455a10df7c8121
graywolf64 <tairikuookami@foxmail.com>
2023-02-05 16:07:33 +0800

    Simplified vars.

--- lua/log4g/core/lookup/LoggerLookup.lua
+++ lua/log4g/core/lookup/LoggerLookup.lua
@@ -4,13 +4,14 @@
 -- @license Apache License 2.0
 -- @copyright GrayWolf64
 local File = "log4g/server/loggercontext/lookup_logger.json"
+local LoggerLookup = Log4g.Core.Logger.Lookup
 
 --- Add a table item to Logger Lookup whose key is Logger name and its content are the associated names of LoggerContext and file paths of LoggerConfig.
 -- If the Lookup doesn't exist, it will create one and write into it.
 -- @param loggername The name of the Logger to write
 -- @param contextname The name of the LoggerContext to write
 -- @param configfile The string filepath of the Logger's associated LoggerConfig to write
-function Log4g.Core.Logger.Lookup.AddItem(loggername, contextname, configfile)
+function LoggerLookup.AddItem(loggername, contextname, configfile)
 	if not file.Exists(File, "DATA") then
 		file.Write(
 			File,
@@ -35,7 +36,7 @@ end
 
 --- Remove any Logger items whose LoggerContext's name is the given contextname.
 -- @param contextname The LoggerContext's name
-function Log4g.Core.Logger.Lookup.RemoveLoggerViaContext(contextname)
+function LoggerLookup.RemoveLoggerViaContext(contextname)
 	if not file.Exists(File, "DATA") then
 		return
 	end
@@ -56,7 +57,7 @@ end
 --- Remove a Logger item from LoggerLookup.
 -- @param contextname The LoggerContext's name
 -- @param loggername The Logger's name
-function Log4g.Core.Logger.Lookup.RemoveLogger(contextname, loggername)
+function LoggerLookup.RemoveLogger(contextname, loggername)
 	if not file.Exists(File, "DATA") then
 		return
 	end
