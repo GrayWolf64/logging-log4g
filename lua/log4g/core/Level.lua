f0b0f15b6a7ef18efeb58bebe7d4de914a3c2f2c
graywolf64 <tairikuookami@foxmail.com>
2023-02-05 13:16:18 +0800

    Reformat.

--- lua/log4g/core/Level.lua
+++ lua/log4g/core/Level.lua
@@ -5,35 +5,35 @@ local Class = include("log4g/core/impl/MiddleClass.lua")
 local Level = Class("Level")
 
 function Level:Initialize(name, int)
-	self.name = name
-	self.int = int
+    self.name = name
+    self.int = int
 end
 
 function Level:__tostring()
-	return "Level: [name:" .. self.name .. "]" .. "[int:" .. self.int .. "]"
+    return "Level: [name:" .. self.name .. "]" .. "[int:" .. self.int .. "]"
 end
 
 function Level:__eq(lhs, rhs)
-	return lhs == rhs
+    return lhs == rhs
 end
 
 --- Get the Level's name.
 -- @return string name
 function Level:Name()
-	return self.name
+    return self.name
 end
 
 --- Get the Level's intlevel.
 -- @return int intlevel
 function Level:IntLevel()
-	return self.int
+    return self.int
 end
 
 --- Calculate the Level's SHA256 Hash Code.
 -- Convert the Level object to string then use util.SHA256().
 -- @return string hashcode
 function Level:HashCode()
-	return util.SHA256(tostring(self))
+    return util.SHA256(tostring(self))
 end
 
 --- Compares the Level against the Levels passed as arguments and returns true if this level is in between the given levels.
@@ -41,11 +41,11 @@ end
 -- @param maxlevel The Level with maximal intlevel
 -- @return bool isinrange
 function Level:IsInRange(minlevel, maxlevel)
-	if self.int >= minlevel.int and self.int <= maxlevel.int then
-		return true
-	else
-		return false
-	end
+    if self.int >= minlevel.int and self.int <= maxlevel.int then
+        return true
+    else
+        return false
+    end
 end
 
 --- Custom Logging Levels created by users.
@@ -56,7 +56,7 @@ local CustomLevel = CustomLevel or {}
 --- Get the Custom Levels as a table.
 -- @return table customlevel
 function Log4g.Level.GetCustomLevel()
-	return CustomLevel
+    return CustomLevel
 end
 
 --- Standard Logging Levels as a table for use internally.
@@ -71,20 +71,20 @@ end
 -- @field FATAL A severe error that will prevent the game from continuing.
 -- @field OFF No events will be logged.
 local StandardLevel = {
-	ALL = Level:New("ALL", math.huge),
-	TRACE = Level:New("TRACE", 600),
-	DEBUG = Level:New("DEBUG", 500),
-	INFO = Level:New("INFO", 400),
-	WARN = Level:New("WARN", 300),
-	ERROR = Level:New("ERROR", 200),
-	FATAL = Level:New("FATAL", 100),
-	OFF = Level:New("OFF", 0),
+    ALL = Level:New("ALL", math.huge),
+    TRACE = Level:New("TRACE", 600),
+    DEBUG = Level:New("DEBUG", 500),
+    INFO = Level:New("INFO", 400),
+    WARN = Level:New("WARN", 300),
+    ERROR = Level:New("ERROR", 200),
+    FATAL = Level:New("FATAL", 100),
+    OFF = Level:New("OFF", 0),
 }
 
 --- Get the Standard Levels as a table.
 -- @return table standardlevel
 function Log4g.Level.GetStandardLevel()
-	return StandardLevel
+    return StandardLevel
 end
 
 --- Get the Level.
@@ -92,13 +92,13 @@ end
 -- @param name The Level's name
 -- @return object level
 function Log4g.Level.GetLevel(name)
-	if HasKey(StandardLevel, name) then
-		return StandardLevel[name]
-	elseif HasKey(CustomLevel, name) then
-		return CustomLevel[name]
-	else
-		return nil
-	end
+    if HasKey(StandardLevel, name) then
+        return StandardLevel[name]
+    elseif HasKey(CustomLevel, name) then
+        return CustomLevel[name]
+    else
+        return nil
+    end
 end
 
 --- Register a Custom Level.
@@ -107,18 +107,16 @@ end
 -- @param int The Level's intlevel
 -- @return object level
 function Log4g.Level.RegisterCustomLevel(name, int)
-	if #name == 0 or int < 0 or HasKey(StandardLevel, name) then
-		return
-	end
+    if #name == 0 or int < 0 or HasKey(StandardLevel, name) then return end
 
-	if not HasKey(CustomLevel, name) then
-		local level = Level:New(name, int)
-		CustomLevel[name] = level
+    if not HasKey(CustomLevel, name) then
+        local level = Level:New(name, int)
+        CustomLevel[name] = level
 
-		return level
-	else
-		CustomLevel[name].int = int
+        return level
+    else
+        CustomLevel[name].int = int
 
-		return CustomLevel[name]
-	end
+        return CustomLevel[name]
+    end
 end
--- lua/log4g/core/Util.lua
+++ lua/log4g/core/Util.lua
@@ -10,25 +10,21 @@ Log4g.Util = Log4g.Util or {}
 -- @return bool ifhaskey
 -- @return keyfound
 Log4g.Util.HasKey = function(tbl, key)
-	if tbl == nil then
-		return false
-	end
+    if tbl == nil then return false end
 
-	for k, _ in pairs(tbl) do
-		if k == key then
-			return true, k
-		end
-	end
+    for k, _ in pairs(tbl) do
+        if k == key then return true, k end
+    end
 
-	return false
+    return false
 end
 
 --- Add all the string keys in a table to network string table.
 -- @param tbl The table of network strings to add
 Log4g.Util.AddNetworkStrsViaTbl = function(tbl)
-	for _, v in pairs(tbl) do
-		util.AddNetworkString(v)
-	end
+    for _, v in pairs(tbl) do
+        util.AddNetworkString(v)
+    end
 end
 
 --- Write simple compressed data.
@@ -36,35 +32,33 @@ end
 -- @param content The content to compress
 -- @param bits The number of bits for net.WriteUInt() to write the length of compressed binary data
 Log4g.Util.WriteDataSimple = function(content, bits)
-	local bindata = util.Compress(content)
-	local len = #bindata
-	net.WriteUInt(len, bits)
-	net.WriteData(bindata, len)
+    local bindata = util.Compress(content)
+    local len = #bindata
+    net.WriteUInt(len, bits)
+    net.WriteData(bindata, len)
 end
 
 --- Recursively delete a folder's content and eventually delete the folder itself.
 -- @param folder The folder to delete which doesn't end with "/"
 -- @param path File search path
 Log4g.Util.DeleteFolderRecursive = function(folder, path)
-	if not file.Exists(folder, path) then
-		return
-	end
-	local files, folders = file.Find(folder .. "/*", path)
+    if not file.Exists(folder, path) then return end
+    local files, folders = file.Find(folder .. "/*", path)
 
-	if istable(files) and not table.IsEmpty(files) then
-		for _, v in pairs(files) do
-			file.Delete(folder .. "/" .. v)
-		end
-	end
+    if istable(files) and not table.IsEmpty(files) then
+        for _, v in pairs(files) do
+            file.Delete(folder .. "/" .. v)
+        end
+    end
 
-	if istable(folders) and not table.IsEmpty(folders) then
-		local DeleteFolderRecursive = Log4g.Util.DeleteFolderRecursive
+    if istable(folders) and not table.IsEmpty(folders) then
+        local DeleteFolderRecursive = Log4g.Util.DeleteFolderRecursive
 
-		for _, j in pairs(folders) do
-			DeleteFolderRecursive(folder .. "/" .. j, path)
-			file.Delete(folder .. "/" .. j)
-		end
-	end
+        for _, j in pairs(folders) do
+            DeleteFolderRecursive(folder .. "/" .. j, path)
+            file.Delete(folder .. "/" .. j)
+        end
+    end
 
-	file.Delete(folder)
+    file.Delete(folder)
 end
