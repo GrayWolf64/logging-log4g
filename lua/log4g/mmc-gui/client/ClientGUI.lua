82ad35a8a578f1c16e1c432e38baa389327e6a92
graywolf64 <tairikuookami@foxmail.com>
2023-02-11 23:04:30 +0800

    Ident to spaces.

--- lua/log4g/mmc-gui/client/ClientGUI.lua
+++ lua/log4g/mmc-gui/client/ClientGUI.lua
@@ -13,255 +13,255 @@ CreateClientConVar("Log4g_CL_GUI_UpdateInterval", 5, true, false, nil, 2, 10)
 local Frame = nil
 
 concommand.Add("Log4g_MMC", function()
-	if IsValid(Frame) then
-		Frame:Remove()
+    if IsValid(Frame) then
+        Frame:Remove()
 
-		return
-	end
-	local UpdateInterval = GetConVar("Log4g_CL_GUI_UpdateInterval"):GetInt()
-	local function GetGameInfo()
-		return "Server: " .. game.GetIPAddress() .. " " .. "SinglePlayer: " .. tostring(game.SinglePlayer())
-	end
+        return
+    end
+    local UpdateInterval = GetConVar("Log4g_CL_GUI_UpdateInterval"):GetInt()
+    local function GetGameInfo()
+        return "Server: " .. game.GetIPAddress() .. " " .. "SinglePlayer: " .. tostring(game.SinglePlayer())
+    end
 
-	Frame = CreateDFrame(
-		770,
-		440,
-		"Log4g Monitoring & Management Console" .. " - " .. GetGameInfo(),
-		"icon16/application.png",
-		nil
-	)
-	local MenuBar = vgui.Create("DMenuBar", Frame)
-	local Icon = vgui.Create("DImageButton", MenuBar)
-	Icon:Dock(RIGHT)
-	Icon:DockMargin(4, 4, 4, 4)
+    Frame = CreateDFrame(
+        770,
+        440,
+        "Log4g Monitoring & Management Console" .. " - " .. GetGameInfo(),
+        "icon16/application.png",
+        nil
+    )
+    local MenuBar = vgui.Create("DMenuBar", Frame)
+    local Icon = vgui.Create("DImageButton", MenuBar)
+    Icon:Dock(RIGHT)
+    Icon:DockMargin(4, 4, 4, 4)
 
-	local function SendEmptyMsgToSV(start)
-		net.Start(start)
-		net.SendToServer()
-	end
+    local function SendEmptyMsgToSV(start)
+        net.Start(start)
+        net.SendToServer()
+    end
 
-	PanelTimedFunc(Icon, UpdateInterval, function() end, function()
-		Icon:SetImage("icon16/disconnect.png")
-		SendEmptyMsgToSV("Log4g_CLReq_ChkConnected")
+    PanelTimedFunc(Icon, UpdateInterval, function() end, function()
+        Icon:SetImage("icon16/disconnect.png")
+        SendEmptyMsgToSV("Log4g_CLReq_ChkConnected")
 
-		net.Receive("Log4g_CLRcv_ChkConnected", function()
-			if not net.ReadBool() then
-				return
-			end
-			Icon:SetImage("icon16/connect.png")
-		end)
-	end)
+        net.Receive("Log4g_CLRcv_ChkConnected", function()
+            if not net.ReadBool() then
+                return
+            end
+            Icon:SetImage("icon16/connect.png")
+        end)
+    end)
 
-	Icon:SetKeepAspect(true)
-	Icon:SetSize(16, 16)
-	local MenuA = MenuBar:AddMenu("New")
-	local MenuB = MenuBar:AddMenu("Options")
-	MenuB:AddOption("General", function() end):SetIcon("icon16/wrench.png")
-	local SheetA = CreateDPropertySheet(Frame, FILL, 0, 1, 0, 0, 4)
-	local SheetPanelA = vgui.Create("DPanel", SheetA)
-	SheetPanelA.Paint = nil
-	SheetA:AddSheet("Configuration", SheetPanelA, "icon16/cog.png")
-	local SheetB = CreateDPropertySheet(SheetPanelA, FILL, 0, 0, 0, 0, 4)
-	local SheetPanelB = vgui.Create("DPanel", SheetB)
-	SheetB:AddSheet("LoggerConfig", SheetPanelB)
-	local ListView = CreateDListView(SheetPanelB, LEFT, 0, 0, 0, 0, 18, 18.5)
-	ListView:SetWide(600)
-	local Tree = vgui.Create("DTree", SheetPanelB)
-	Tree:Dock(RIGHT)
-	Tree:SetWide(140)
-	Tree:DockMargin(0, 0, 0, 0)
+    Icon:SetKeepAspect(true)
+    Icon:SetSize(16, 16)
+    local MenuA = MenuBar:AddMenu("New")
+    local MenuB = MenuBar:AddMenu("Options")
+    MenuB:AddOption("General", function() end):SetIcon("icon16/wrench.png")
+    local SheetA = CreateDPropertySheet(Frame, FILL, 0, 1, 0, 0, 4)
+    local SheetPanelA = vgui.Create("DPanel", SheetA)
+    SheetPanelA.Paint = nil
+    SheetA:AddSheet("Configuration", SheetPanelA, "icon16/cog.png")
+    local SheetB = CreateDPropertySheet(SheetPanelA, FILL, 0, 0, 0, 0, 4)
+    local SheetPanelB = vgui.Create("DPanel", SheetB)
+    SheetB:AddSheet("LoggerConfig", SheetPanelB)
+    local ListView = CreateDListView(SheetPanelB, LEFT, 0, 0, 0, 0, 18, 18.5)
+    ListView:SetWide(600)
+    local Tree = vgui.Create("DTree", SheetPanelB)
+    Tree:Dock(RIGHT)
+    Tree:SetWide(140)
+    Tree:DockMargin(0, 0, 0, 0)
 
-	for _, v in pairs({ "name", "loggercontext", "level", "appender", "layout" }) do
-		ListView:AddColumn(v)
-	end
+    for _, v in pairs({ "name", "loggercontext", "level", "appender", "layout" }) do
+        ListView:AddColumn(v)
+    end
 
-	--- Start a special net msg for ListView's line behaviour after being clicked on.
-	-- @lfunction NetStrMsgSpecial
-	-- @param num The number of the line
-	-- @param listview The DListView containing the line
-	-- @param start The net msg to start
-	-- @param ... The texts of the specific column headers
-	local function NetStrMsgSpecial(num, listview, start, ...)
-		local args = { ... }
+    --- Start a special net msg for ListView's line behaviour after being clicked on.
+    -- @lfunction NetStrMsgSpecial
+    -- @param num The number of the line
+    -- @param listview The DListView containing the line
+    -- @param start The net msg to start
+    -- @param ... The texts of the specific column headers
+    local function NetStrMsgSpecial(num, listview, start, ...)
+        local args = { ... }
 
-		local result = GetColumnSpecialText(num, listview, unpack(args))
-		net.Start(start)
+        local result = GetColumnSpecialText(num, listview, unpack(args))
+        net.Start(start)
 
-		for _, v in ipairs(args) do
-			net.WriteString(result[v])
-		end
+        for _, v in ipairs(args) do
+            net.WriteString(result[v])
+        end
 
-		net.SendToServer()
-	end
+        net.SendToServer()
+    end
 
-	PanelTimedFunc(ListView, UpdateInterval, function() end, function()
-		SendEmptyMsgToSV("Log4g_CLReq_LoggerConfigs")
+    PanelTimedFunc(ListView, UpdateInterval, function() end, function()
+        SendEmptyMsgToSV("Log4g_CLReq_LoggerConfigs")
 
-		net.Receive("Log4g_CLRcv_LoggerConfigs", function()
-			ListView:Clear()
-			if net.ReadBool() then
-				for _, v in ipairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
-					local Line = ListView:AddLine()
-					SetProperLineText(v, Line, ListView)
-				end
-			end
-		end)
-	end)
+        net.Receive("Log4g_CLRcv_LoggerConfigs", function()
+            ListView:Clear()
+            if net.ReadBool() then
+                for _, v in ipairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
+                    local Line = ListView:AddLine()
+                    SetProperLineText(v, Line, ListView)
+                end
+            end
+        end)
+    end)
 
-	local SheetPanelBA = vgui.Create("DPanel", SheetB)
-	SheetB:AddSheet("LoggerContext", SheetPanelBA)
-	local ListViewC = vgui.Create("DListView", SheetPanelBA)
-	ListViewC:Dock(FILL)
-	for _, v in pairs({ "name" }) do
-		ListViewC:AddColumn(v)
-	end
-	function ListViewC:OnRowRightClick(num)
-		local Menu = DermaMenu()
+    local SheetPanelBA = vgui.Create("DPanel", SheetB)
+    SheetB:AddSheet("LoggerContext", SheetPanelBA)
+    local ListViewC = vgui.Create("DListView", SheetPanelBA)
+    ListViewC:Dock(FILL)
+    for _, v in pairs({ "name" }) do
+        ListViewC:AddColumn(v)
+    end
+    function ListViewC:OnRowRightClick(num)
+        local Menu = DermaMenu()
 
-		Menu:AddOption("Terminate", function()
-			NetStrMsgSpecial(num, ListViewC, "Log4g_CLReq_LoggerContext_Terminate", "name")
-		end):SetIcon("icon16/cross.png")
+        Menu:AddOption("Terminate", function()
+            NetStrMsgSpecial(num, ListViewC, "Log4g_CLReq_LoggerContext_Terminate", "name")
+        end):SetIcon("icon16/cross.png")
 
-		Menu:Open()
-	end
-	PanelTimedFunc(ListViewC, UpdateInterval, function() end, function()
-		SendEmptyMsgToSV("Log4g_CLReq_LoggerContext_Lookup")
+        Menu:Open()
+    end
+    PanelTimedFunc(ListViewC, UpdateInterval, function() end, function()
+        SendEmptyMsgToSV("Log4g_CLReq_LoggerContext_Lookup")
 
-		net.Receive("Log4g_CLRcv_LoggerContext_Lookup", function()
-			ListViewC:Clear()
-			if net.ReadBool() then
-				for k, _ in pairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
-					ListViewC:AddLine(k)
-				end
-			end
-		end)
-	end)
+        net.Receive("Log4g_CLRcv_LoggerContext_Lookup", function()
+            ListViewC:Clear()
+            if net.ReadBool() then
+                for k, _ in pairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
+                    ListViewC:AddLine(k)
+                end
+            end
+        end)
+    end)
 
-	local SubB = MenuB:AddSubMenu("View")
-	SubB:SetDeleteSelf(false)
+    local SubB = MenuB:AddSubMenu("View")
+    SubB:SetDeleteSelf(false)
 
-	SubB:AddOption("Clear", function()
-		ListView:Clear()
-		Tree:Clear()
-	end):SetIcon("icon16/application_form_delete.png")
+    SubB:AddOption("Clear", function()
+        ListView:Clear()
+        Tree:Clear()
+    end):SetIcon("icon16/application_form_delete.png")
 
-	PanelTimedFunc(Tree, UpdateInterval, function() end, function()
-		SendEmptyMsgToSV("Log4g_CLReq_LoggerConfig_Lookup")
+    PanelTimedFunc(Tree, UpdateInterval, function() end, function()
+        SendEmptyMsgToSV("Log4g_CLReq_LoggerConfig_Lookup")
 
-		net.Receive("Log4g_CLRcv_LoggerConfig_Lookup", function()
-			Tree:Clear()
-			if net.ReadBool() then
-				for k, _ in pairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
-					Tree:AddNode(k, "icon16/brick.png")
-				end
-			end
-		end)
-	end)
+        net.Receive("Log4g_CLRcv_LoggerConfig_Lookup", function()
+            Tree:Clear()
+            if net.ReadBool() then
+                for k, _ in pairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
+                    Tree:AddNode(k, "icon16/brick.png")
+                end
+            end
+        end)
+    end)
 
-	local SubMenuB = MenuA:AddSubMenu("Configuration")
-	SubMenuB:SetDeleteSelf(false)
+    local SubMenuB = MenuA:AddSubMenu("Configuration")
+    SubMenuB:SetDeleteSelf(false)
 
-	SubMenuB:AddOption("Level", function()
-		local Window = CreateDFrame(300, 150, "New Level", "icon16/application.png", Frame)
-		Window:SetDrawOnTop(true)
-		local DProp = vgui.Create("DProperties", Window)
-		DProp:Dock(FILL)
-		local RowA, RowB =
-			CreateDPropRow(DProp, "Self", "Name", "Generic"), CreateDPropRow(DProp, "Self", "IntLevel", "Generic")
-		local ButtonB = CreateDButton(Window, BOTTOM, 100, 0, 100, 0, 100, 50, "Submit")
+    SubMenuB:AddOption("Level", function()
+        local Window = CreateDFrame(300, 150, "New Level", "icon16/application.png", Frame)
+        Window:SetDrawOnTop(true)
+        local DProp = vgui.Create("DProperties", Window)
+        DProp:Dock(FILL)
+        local RowA, RowB =
+            CreateDPropRow(DProp, "Self", "Name", "Generic"), CreateDPropRow(DProp, "Self", "IntLevel", "Generic")
+        local ButtonB = CreateDButton(Window, BOTTOM, 100, 0, 100, 0, 100, 50, "Submit")
 
-		ButtonB.DoClick = function()
-			local InputName = GetRowControlValue(RowA)
-			local InputInt = GetRowControlValue(RowB)
-			if #InputName == 0 or #InputInt == 0 then
-				return
-			end
-			net.Start("Log4g_CLUpload_NewLevel")
-			net.WriteString(InputName)
-			net.WriteUInt(tonumber(InputInt), 16)
-			net.SendToServer()
-			Window:Close()
-		end
-	end):SetIcon("icon16/chart_bar.png")
+        ButtonB.DoClick = function()
+            local InputName = GetRowControlValue(RowA)
+            local InputInt = GetRowControlValue(RowB)
+            if #InputName == 0 or #InputInt == 0 then
+                return
+            end
+            net.Start("Log4g_CLUpload_NewLevel")
+            net.WriteString(InputName)
+            net.WriteUInt(tonumber(InputInt), 16)
+            net.SendToServer()
+            Window:Close()
+        end
+    end):SetIcon("icon16/chart_bar.png")
 
-	local SheetPanelC = vgui.Create("DPanel", SheetA)
-	SheetA:AddSheet("Overview (SV)", SheetPanelC, "icon16/page.png")
-	local SheetPanelD = vgui.Create("DPanel", SheetA)
-	SheetA:AddSheet("Summary", SheetPanelD, "icon16/table.png")
-	local SummarySheet = vgui.Create("DProperties", SheetPanelD)
-	SummarySheet:Dock(FILL)
+    local SheetPanelC = vgui.Create("DPanel", SheetA)
+    SheetA:AddSheet("Overview (SV)", SheetPanelC, "icon16/page.png")
+    local SheetPanelD = vgui.Create("DPanel", SheetA)
+    SheetA:AddSheet("Summary", SheetPanelD, "icon16/table.png")
+    local SummarySheet = vgui.Create("DProperties", SheetPanelD)
+    SummarySheet:Dock(FILL)
 
-	--- Create a row with a Generic RowControl which users can't type into inside SummarySheet.
-	local function CreateSpecialRow(category, name)
-		local control = GetRowControl(CreateDPropRow(SummarySheet, category, name, "Generic"))
-		control:SetEditable(false)
-		return control
-	end
+    --- Create a row with a Generic RowControl which users can't type into inside SummarySheet.
+    local function CreateSpecialRow(category, name)
+        local control = GetRowControl(CreateDPropRow(SummarySheet, category, name, "Generic"))
+        control:SetEditable(false)
+        return control
+    end
 
-	local RowA, RowB, RowC, RowD =
-		CreateSpecialRow("Client", "OS Date"),
-		CreateSpecialRow("Server", "Estimated Tickrate"),
-		CreateSpecialRow("Server", "Floored Lua Dynamic RAM Usage (kB)"),
-		CreateSpecialRow("Server", "Entity Count")
-	local RowE, RowF, RowG, RowH =
-		CreateSpecialRow("Server", "Networked Entity (EDICT) Count"),
-		CreateSpecialRow("Server", "Net Receiver Count"),
-		CreateSpecialRow("Server", "Lua Registry Table Element Count"),
-		CreateSpecialRow("Server", "Constraint Count")
+    local RowA, RowB, RowC, RowD =
+        CreateSpecialRow("Client", "OS Date"),
+        CreateSpecialRow("Server", "Estimated Tickrate"),
+        CreateSpecialRow("Server", "Floored Lua Dynamic RAM Usage (kB)"),
+        CreateSpecialRow("Server", "Entity Count")
+    local RowE, RowF, RowG, RowH =
+        CreateSpecialRow("Server", "Networked Entity (EDICT) Count"),
+        CreateSpecialRow("Server", "Net Receiver Count"),
+        CreateSpecialRow("Server", "Lua Registry Table Element Count"),
+        CreateSpecialRow("Server", "Constraint Count")
 
-	local function UpdateSummary()
-		SendEmptyMsgToSV("Log4g_CLReq_SVSummaryData")
+    local function UpdateSummary()
+        SendEmptyMsgToSV("Log4g_CLReq_SVSummaryData")
 
-		net.Receive("Log4g_CLRcv_SVSummaryData", function()
-			RowB:SetValue(tostring(1 / engine.ServerFrameTime()))
-			RowC:SetValue(tostring(net.ReadFloat()))
-			RowD:SetValue(tostring(net.ReadUInt(14)))
-			RowE:SetValue(tostring(net.ReadUInt(13)))
-			RowF:SetValue(tostring(net.ReadUInt(12)))
-			RowG:SetValue(tostring(net.ReadUInt(32)))
-			RowH:SetValue(tostring(net.ReadUInt(16)))
-		end)
-	end
+        net.Receive("Log4g_CLRcv_SVSummaryData", function()
+            RowB:SetValue(tostring(1 / engine.ServerFrameTime()))
+            RowC:SetValue(tostring(net.ReadFloat()))
+            RowD:SetValue(tostring(net.ReadUInt(14)))
+            RowE:SetValue(tostring(net.ReadUInt(13)))
+            RowF:SetValue(tostring(net.ReadUInt(12)))
+            RowG:SetValue(tostring(net.ReadUInt(32)))
+            RowH:SetValue(tostring(net.ReadUInt(16)))
+        end)
+    end
 
-	PanelTimedFunc(SummarySheet, UpdateInterval, function()
-		RowA:SetValue(tostring(os.date()))
-	end, function()
-		UpdateSummary()
-	end)
+    PanelTimedFunc(SummarySheet, UpdateInterval, function()
+        RowA:SetValue(tostring(os.date()))
+    end, function()
+        UpdateSummary()
+    end)
 
-	UpdateSummary()
-	local SheetPanelE = vgui.Create("DPanel", SheetA)
-	SheetA:AddSheet("LOGGER", SheetPanelE, "icon16/brick.png")
-	local ListViewB = CreateDListView(SheetPanelE, FILL, 0, 0, 0, 0, 18, 18.5)
+    UpdateSummary()
+    local SheetPanelE = vgui.Create("DPanel", SheetA)
+    SheetA:AddSheet("LOGGER", SheetPanelE, "icon16/brick.png")
+    local ListViewB = CreateDListView(SheetPanelE, FILL, 0, 0, 0, 0, 18, 18.5)
 
-	for _, v in pairs({ "name", "loggercontext" }) do
-		ListViewB:AddColumn(v)
-	end
+    for _, v in pairs({ "name", "loggercontext" }) do
+        ListViewB:AddColumn(v)
+    end
 
-	function ListViewB:OnRowRightClick(num)
-		local Menu = DermaMenu()
+    function ListViewB:OnRowRightClick(num)
+        local Menu = DermaMenu()
 
-		Menu:AddOption("Terminate", function()
-			NetStrMsgSpecial(num, ListViewB, "Log4g_CLReq_Logger_Remove", "loggercontext", "name")
-		end):SetIcon("icon16/cross.png")
+        Menu:AddOption("Terminate", function()
+            NetStrMsgSpecial(num, ListViewB, "Log4g_CLReq_Logger_Remove", "loggercontext", "name")
+        end):SetIcon("icon16/cross.png")
 
-		Menu:Open()
-	end
+        Menu:Open()
+    end
 
-	PanelTimedFunc(ListViewB, UpdateInterval, function() end, function()
-		SendEmptyMsgToSV("Log4g_CLReq_Logger_Lookup")
+    PanelTimedFunc(ListViewB, UpdateInterval, function() end, function()
+        SendEmptyMsgToSV("Log4g_CLReq_Logger_Lookup")
 
-		net.Receive("Log4g_CLRcv_Logger_Lookup", function()
-			ListViewB:Clear()
-			if net.ReadBool() then
-				for k, v in pairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
-					local Line = ListViewB:AddLine()
+        net.Receive("Log4g_CLRcv_Logger_Lookup", function()
+            ListViewB:Clear()
+            if net.ReadBool() then
+                for k, v in pairs(util.JSONToTable(util.Decompress(net.ReadData(net.ReadUInt(16))))) do
+                    local Line = ListViewB:AddLine()
 
-					SetProperLineText({
-						name = k,
-					}, Line, ListViewB)
-				end
-			end
-		end)
-	end)
+                    SetProperLineText({
+                        name = k,
+                    }, Line, ListViewB)
+                end
+            end
+        end)
+    end)
 end)
--- lua/log4g/mmc-gui/client/ClientGUIDerma.lua
+++ lua/log4g/mmc-gui/client/ClientGUIDerma.lua
@@ -16,16 +16,6 @@ function ClientGUIDerma.CreateDFrame(a, b, title, icon, parent)
     return dframe
 end
 
-function ClientGUIDerma.CreateDButton(parent, docktype, x, y, z, w, a, b, text)
-    local dbutton = vgui.Create("DButton", parent)
-    dbutton:Dock(docktype)
-    dbutton:DockMargin(x, y, z, w)
-    dbutton:SetSize(a, b)
-    dbutton:SetText(text)
-
-    return dbutton
-end
-
 function ClientGUIDerma.CreateDListView(parent, docktype, x, y, z, w, ha, hb)
     local dlistview = vgui.Create("DListView", parent)
     dlistview:SetMultiSelect(false)
@@ -65,20 +55,6 @@ end
 -- @return row obtained row
 function ClientGUIDerma.GetRowControl(row)
     return row:GetChild(1):GetChild(0):GetChild(0)
-end
-
---- Get a RowControl's value (edited by user) whether it's a DTextEntry or a DComboBox.
--- @param row The row in the DProp Panel
--- @return string value obtained
-function ClientGUIDerma.GetRowControlValue(row)
-    local pnl = GetRowControl(row)
-    local cls = pnl:GetName()
-
-    if cls == "DTextEntry" then
-        return pnl:GetValue()
-    elseif cls == "DComboBox" then
-        return pnl:GetSelected()
-    end
 end
 
 --- In a Panel's Think, run a function and run another function but timed.
