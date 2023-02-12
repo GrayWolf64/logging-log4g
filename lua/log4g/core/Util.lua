351c043bde819deb6db4cbd9305fbdbae51fac3a
graywolf64 <tairikuookami@foxmail.com>
2023-02-12 10:38:10 +0800

    Cleaned Util.

--- lua/log4g/core/Util.lua
+++ lua/log4g/core/Util.lua
@@ -3,7 +3,6 @@
 -- @license Apache License 2.0
 -- @copyright GrayWolf64
 Log4g.Util = Log4g.Util or {}
-local sql = sql
 
 --- Check if one table has a certain key.
 -- @param tbl The table to check
@@ -26,35 +25,4 @@ Log4g.Util.AddNetworkStrsViaTbl = function(tbl)
     for _, v in pairs(tbl) do
         util.AddNetworkString(v)
     end
-end
-
---- Write simple compressed data.
--- Must be used between net start and send.
--- @param content The content to compress
--- @param bits The number of bits for net.WriteUInt() to write the length of compressed binary data
-Log4g.Util.WriteDataSimple = function(content, bits)
-    local bindata = util.Compress(content)
-    local len = #bindata
-    net.WriteUInt(len, bits)
-    net.WriteData(bindata, len)
-end
-
-Log4g.Util.SQLQueryRow = function(tbl, name)
-    return sql.QueryRow("SELECT * FROM " .. tbl .. " WHERE Name = '" .. name .. "';")
-end
-
-Log4g.Util.SQLQueryValue = function(tbl, name)
-    return sql.QueryValue("SELECT Content FROM " .. tbl .. " WHERE Name = '" .. name .. "';")
-end
-
-Log4g.Util.SQLUpdateValue = function(tbl, name, str)
-    sql.Query("UPDATE " .. tbl .. " SET Content = " .. sql.SQLStr(str) .. " WHERE Name = '" .. name .. "';")
-end
-
-Log4g.Util.SQLInsert = function(tbl, name, str)
-    sql.Query("INSERT INTO " .. tbl .. " (Name, Content) VALUES('" .. name .. "', " .. sql.SQLStr(str) .. ")")
-end
-
-Log4g.Util.SQLDeleteRow = function(tbl, name)
-    sql.Query("DELETE FROM " .. tbl .. " WHERE Name = '" .. name .. "';")
 end
