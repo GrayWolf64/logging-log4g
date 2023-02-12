5b3427571b72b08b77c4c3d3f66d32725a9850da
graywolf64 <tairikuookami@foxmail.com>
2023-02-12 09:24:47 +0800

    Fixed TestLogger.

--- lua/log4g/core-test/TestLogger.lua
+++ lua/log4g/core-test/TestLogger.lua
@@ -1,9 +1,13 @@
 concommand.Add("Log4g_CoreTest_CreateLogger", function()
     Log4g.API.LogManager.GetLogger("TestLoggerA")
-    Log4g.Core.Logger.Get("TestLoggerA"):SetLevel(Log4g.Level.GetStandardLevel().INFO)
+    Log4g.Core.Logger.Get("TestLoggerA"):SetLevel(Log4g.Level.GetStdLevel().INFO)
     Log4g.Core.Logger.Get("TestLoggerA"):INFO("Test message from 'TestLoggerA'\n")
 end)
 
 concommand.Add("Log4g_CoreTest_ShowAllLogger", function()
     PrintTable(Log4g.Core.Logger.GetAll())
+end)
+
+concommand.Add("Log4g_CoreTest_ShowAllLoggerConfig", function()
+    PrintTable(Log4g.Core.Config.LoggerConfig.GetAll())
 end)
