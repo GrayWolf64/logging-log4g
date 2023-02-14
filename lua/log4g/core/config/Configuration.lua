b0264791a422d70636b0a384dedf70594df149b1
graywolf64 <tairikuookami@foxmail.com>
2023-02-14 21:59:33 +0800

    Register Configuration.

--- lua/log4g/core/config/Configuration.lua
+++ lua/log4g/core/config/Configuration.lua
@@ -3,6 +3,10 @@
 local Class = include("log4g/core/impl/MiddleClass.lua")
 local Configuration = Class("Configuration")
 
-function Configuration:Initialize(name)
-    self.name = name
+function Configuration:Initialize()
+end
+
+--- Register a Configuration for the LoggerContext.
+function Log4g.Core.Config.Register(context)
+    context.configuration = Configuration:New()
 end
