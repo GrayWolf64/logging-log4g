7a7588d8d9b7ca7137f7f01e2f1804d0a8047e60
graywolf64 <tairikuookami@foxmail.com>
2023-02-16 23:48:47 +0800

    Make some attributes of LoggerContext private.

--- lua/log4g/core/LoggerContext.lua
+++ lua/log4g/core/LoggerContext.lua
@@ -6,13 +6,29 @@ local HasKey = Log4g.Util.HasKey
 local RemoveLoggerConfigByContext = Log4g.Core.Config.LoggerConfig.RemoveByContext
 local SetState = Log4g.Core.LifeCycle.SetState
 local INITIALIZING, INITIALIZED = Log4g.Core.LifeCycle.State.INITIALIZING, Log4g.Core.LifeCycle.State.INITIALIZED
-local STARTING, STARTED = Log4g.Core.LifeCycle.State.STARTING, Log4g.Core.LifeCycle.State.STARTED
 local STOPPING, STOPPED = Log4g.Core.LifeCycle.State.STOPPING, Log4g.Core.LifeCycle.State.STOPPED
 local RegisterConfiguration = Log4g.Core.Config.Configuration.Register
+--- This is where all the LoggerContexts are stored.
+-- LoggerContexts may include some Loggers which may also include Appender, Level objects and so on.
+-- @local
+-- @table INSTANCES
+local INSTANCES = INSTANCES or {}
+
+--- A weak table which stores some private attributes of the LoggerContext object.
+-- @local
+-- @table PRIVATE
+local PRIVATE = setmetatable({}, {
+    __mode = "k"
+})
 
 function LoggerContext:Initialize(name)
     SetState(self, INITIALIZING)
     self.name = name
+
+    PRIVATE[self] = {
+        configuration = RegisterConfiguration()
+    }
+
     SetState(self, INITIALIZED)
 end
 
@@ -22,27 +38,20 @@ function LoggerContext:GetName()
     return self.name
 end
 
---- Start the LoggerContext with a specific configuration.
--- @param config configuration
-function LoggerContext:Start(config)
-    SetState(self, STARTING)
-    self.configuration = config
-    SetState(self, STARTED)
+--- Returns the current Configuration of the LoggerContext.
+-- @return object configuration
+function LoggerContext:GetConfiguration()
+    return PRIVATE[self].configuration
 end
 
 function LoggerContext:__tostring()
     return "LoggerContext: [name:" .. self.name .. "]"
 end
 
---- This is where all the LoggerContexts are stored.
--- LoggerContexts may include some Loggers which may also include Appender, Level objects and so on.
--- @local
--- @table INSTANCES
-local INSTANCES = INSTANCES or {}
-
 --- Terminate the LoggerContext.
 function LoggerContext:Terminate()
     SetState(self, STOPPING)
+    PRIVATE[self] = nil
     RemoveLoggerConfigByContext(self.name)
     SetState(self, STOPPED)
     hook.Run("Log4g_PostLoggerContextTermination")
@@ -67,7 +76,6 @@ end
 function Log4g.Core.LoggerContext.Register(name)
     if not HasKey(INSTANCES, name) then
         INSTANCES[name] = LoggerContext:New(name)
-        INSTANCES[name].configuration = RegisterConfiguration()
         hook.Run("Log4g_PostLoggerContextRegistration", name)
 
         return INSTANCES[name]
