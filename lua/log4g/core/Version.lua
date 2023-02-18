e039a45358396cfaf06c1a251999af12559dddd9
graywolf64 <tairikuookami@foxmail.com>
2023-02-18 19:55:55 +0800

    Simple version check.

--- lua/log4g/core/Version.lua
+++ lua/log4g/core/Version.lua
@@ -3,5 +3,9 @@
 Log4g._VERSION = Log4g._VERSION or {}
 
 concommand.Add("Log4g_Version", function()
-    print("Version Checked.")
+    http.Fetch("https://raw.githubusercontent.com/GrayWolf64/gmod-logging-log4g/main/VERSION.txt", function(body)
+        print(body)
+    end, function(msg)
+        print(msg)
+    end)
 end)
