19b79c4751034bd1baa11e31267e09f4a31b5dbe
graywolf64 <tairikuookami@foxmail.com>
2023-02-18 19:47:26 +0800

    Version related.

--- lua/log4g/core/Version.lua
+++ lua/log4g/core/Version.lua
@@ -1,4 +1,7 @@
 --- Version handler.
 -- @script Version
 Log4g._VERSION = Log4g._VERSION or {}
-concommand.Add("Log4g_VERSION", function() end)
+
+concommand.Add("Log4g_Version", function()
+    print("Version Checked.")
+end)
