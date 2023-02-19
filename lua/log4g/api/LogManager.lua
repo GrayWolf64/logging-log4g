9fcd112b49ec026128239e025f12a91a5bc58933
graywolf64 <tairikuookami@foxmail.com>
2023-02-19 11:39:45 +0800

    API.Exists

--- lua/log4g/api/LogManager.lua
+++ lua/log4g/api/LogManager.lua
@@ -1,17 +1,17 @@
 --- The anchor point for the Log4g logging system.
 -- @script LogManager
-Log4g.API.LogManager = Log4g.API.LogManager or {
-    RootLoggerName = "Root"
-}
-
+Log4g.API.LogManager = Log4g.API.LogManager or {}
+Log4g.API.LogManager.RootLoggerName = "Root"
 local API = Log4g.API.LogManager
-local GetAllLoggers = Log4g.Core.Logger.GetAll
+local GetAllContexts = Log4g.Core.LoggerContext.GetAll
 
 --- Detects if a Logger with the specified name exists.
 -- @param name The name of the Logger
 -- @return bool haslogger
 function API.Exists(name)
-    if GetAllLoggers()[name] then return true end
+    for _, v in pairs(GetAllContexts()) do
+        if v:HasLogger(name) then return true end
+    end
 
     return false
 end
