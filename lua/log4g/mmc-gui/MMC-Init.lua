f30551eb532c6b6c4180f164db2bcf45ac695cf7
graywolf64 <tairikuookami@foxmail.com>
2023-02-24 12:03:39 +0800

    Combined Configurator and Management. We really don't need to separate them.

--- lua/log4g/mmc-gui/MMC-Init.lua
+++ lua/log4g/mmc-gui/MMC-Init.lua
@@ -2,8 +2,7 @@
 -- It's based on [Derma](https://wiki.facepunch.com/gmod/Derma_Basic_Guide) UI system that comes with the game itself.
 -- @script MMC-Init
 if SERVER then
-    include("log4g/mmc-gui/server/ClientGUIConfigurator.lua")
-    include("log4g/mmc-gui/server/ClientGUISummaryData.lua")
+    include("log4g/mmc-gui/server/ClientGUI.lua")
     AddCSLuaFile("log4g/mmc-gui/client/ClientGUIDerma.lua")
     AddCSLuaFile("log4g/mmc-gui/client/ClientGUI.lua")
 elseif CLIENT then
--- lua/log4g/mmc-gui/server/ClientGUI.lua
+++ lua/log4g/mmc-gui/server/ClientGUI.lua
@@ -1,9 +1,21 @@
+--- Server-side processing of the Client GUI requests.
+-- @script ClientGUI
+-- @license Apache License 2.0
+-- @copyright GrayWolf64
 local AddNetworkStrsViaTbl = Log4g.Util.AddNetworkStrsViaTbl
 
 AddNetworkStrsViaTbl({
-    [1] = "Log4g_CLReq_SVSummaryData",
-    [2] = "Log4g_CLRcv_SVSummaryData",
+    [1] = "Log4g_CLReq_ChkConnected",
+    [2] = "Log4g_CLRcv_ChkConnected",
+    [3] = "Log4g_CLReq_SVSummaryData",
+    [4] = "Log4g_CLRcv_SVSummaryData",
 })
+
+net.Receive("Log4g_CLReq_ChkConnected", function(_, ply)
+    net.Start("Log4g_CLRcv_ChkConnected")
+    net.WriteBool(IsValid(ply) == ply:IsConnected() == true)
+    net.Send(ply)
+end)
 
 net.Receive("Log4g_CLReq_SVSummaryData", function(_, ply)
     net.Start("Log4g_CLRcv_SVSummaryData")
