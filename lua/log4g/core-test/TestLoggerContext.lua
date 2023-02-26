33fef122c2e05897e1fcf2e5ebc2273ec2fb6c98
graywolf64 <tairikuookami@foxmail.com>
2023-02-26 21:15:43 +0800

    Changed test.

--- lua/log4g/core-test/TestLoggerContext.lua
+++ lua/log4g/core-test/TestLoggerContext.lua
@@ -1,14 +1,9 @@
-local function CreateContext()
+concommand.Add("Log4g_CoreTest_CreateLoggerContext", function()
     local GetContext = Log4g.API.LoggerContextFactory.GetContext
-    GetContext(CreateContext)
 
     for i = 1, 10 do
         GetContext(tostring(i))
     end
-end
-
-concommand.Add("Log4g_CoreTest_CreateLoggerContext", function()
-    CreateContext()
 end)
 
 concommand.Add("Log4g_CoreTest_RemoveLoggerContext", function()
