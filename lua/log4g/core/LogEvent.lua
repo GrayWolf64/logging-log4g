85be4d674612e79a9e4b5de7ab03289246de9303
graywolf64 <tairikuookami@foxmail.com>
2023-03-04 12:06:28 +0800

    Some minor updates.

--- lua/log4g/core/LogEvent.lua
+++ lua/log4g/core/LogEvent.lua
@@ -4,5 +4,11 @@
 Log4g.Core.LogEvent = Log4g.Core.LogEvent or {}
 local LogEvent = include("log4g/core/impl/MiddleClass.lua")("LogEvent")
 
-function LogEvent:Initialize()
+function LogEvent:Initialize(lname, level)
+    self.lname = lname
+    self.level = level
+end
+
+function Log4g.Core.LogEvent.Builder(lname, level)
+    return LogEvent(lname, level)
 end
