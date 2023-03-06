899acce0bf29178794e366094f04404273fd1452
graywolf64 <tairikuookami@foxmail.com>
2023-03-06 21:45:33 +0800

    Make `IsNameSafe` a util function called `QualifyName`.

--- lua/log4g/core/Util.lua
+++ lua/log4g/core/Util.lua
@@ -4,6 +4,7 @@
 -- @copyright GrayWolf64
 Log4g.Util = Log4g.Util or {}
 local Util = Log4g.Util
+local STRL, STRR = string.Left, string.Right
 
 --- Add all the string keys in a table to network string table.
 -- @param tbl The table of network strings to add
@@ -13,4 +14,13 @@ function Util.AddNetworkStrsViaTbl(tbl)
     for _, v in pairs(tbl) do
         AddNetworkString(v)
     end
+end
+
+--- Qualifies the string name of an object and returns if it's a valid name.
+-- @param str String name
+-- @return bool ifvalid
+function Util.QualifyName(str)
+    if STRL(str, 1) == "." or STRR(str, 1) == "." or string.find(str, "[^%a%.]") then return false end
+
+    return true
 end
