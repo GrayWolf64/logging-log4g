9e417779144a130ff9a8ca834bbb9daaef1c65d3
graywolf64 <tairikuookami@foxmail.com>
2023-03-17 18:26:06 +0800

    Simplified `QualifyName`.

--- lua/log4g/core/Util.lua
+++ lua/log4g/core/Util.lua
@@ -4,7 +4,7 @@
 -- @copyright GrayWolf64
 Log4g.Util = Log4g.Util or {}
 local Util = Log4g.Util
-local SLeft, SRight, SFind = string.Left, string.Right, string.find
+local SSub, SFind = string.sub, string.find
 local isstring = isstring
 
 --- Add all the string keys in a table to network string table.
@@ -21,7 +21,7 @@ end
 -- @param str String name
 -- @return bool ifvalid
 function Util.QualifyName(str)
-    if not isstring(str) or SLeft(str, 1) == "." or SRight(str, 1) == "." or SFind(str, "[^%a%.]") then return false end
+    if not isstring(str) or SSub(str, 1, 1) == "." or SSub(str, -1) == "." or SFind(str, "[^%a%.]") then return false end
 
     return true
 end
