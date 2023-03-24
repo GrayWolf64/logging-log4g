93d7888a170a88ee1c1c180c3e2f55111d4e564b
graywolf64 <tairikuookami@foxmail.com>
2023-03-24 22:52:08 +0800

    Removed one `Accessor` naming.

--- lua/log4g/core/config/DefaultConfiguration.lua
+++ lua/log4g/core/config/DefaultConfiguration.lua
@@ -1,5 +1,5 @@
-local Accessor = Log4g.Core.Config
-Accessor.DEFAULT_NAME = "Default"
+Log4g.Core.Config.DEFAULT_NAME = "Default"
+local DEFAULT_NAME = Log4g.Core.Config.DEFAULT_NAME
 local Configuration = Log4g.Core.Config.Configuration.GetClass()
 local DefaultConfiguration = Configuration:subclass("DefaultConfiguration")
 local CreateConsoleAppender, CreatePatternLayout = Log4g.Core.Appender.CreateConsoleAppender, Log4g.Core.Layout.CreatePatternLayout
@@ -9,9 +9,9 @@ function DefaultConfiguration:Initialize(name)
     self.name = name
 end
 
-function Accessor.GetDefaultConfiguration()
-    local configuration = DefaultConfiguration(Accessor.DEFAULT_NAME)
-    configuration:AddAppender(CreateConsoleAppender(Accessor.DEFAULT_NAME .. "Appender", CreatePatternLayout(Accessor.DEFAULT_NAME .. "Layout")))
+function Log4g.Core.Config.GetDefaultConfiguration()
+    local configuration = DefaultConfiguration(DEFAULT_NAME)
+    configuration:AddAppender(CreateConsoleAppender(DEFAULT_NAME .. "Appender", CreatePatternLayout(DEFAULT_NAME .. "Layout")))
 
     return configuration
 end
