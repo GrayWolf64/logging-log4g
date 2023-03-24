3f9c1872aad9ba658c5f06d016b271cc457f5d21
graywolf64 <tairikuookami@foxmail.com>
2023-03-24 23:03:35 +0800

    Renamed one var.

--- lua/log4g/api/LogManager.lua
+++ lua/log4g/api/LogManager.lua
@@ -3,7 +3,7 @@
 Log4g.API.LogManager = Log4g.API.LogManager or {}
 local API = Log4g.API.LogManager
 local GetAllContexts = Log4g.Core.LoggerContext.GetAll
-local TIsEmpty = table.IsEmpty
+local tisempty = table.IsEmpty
 local isstring = isstring
 
 --- Detects if a Logger with the specified name exists.
@@ -11,7 +11,7 @@ local isstring = isstring
 -- @return bool haslogger
 function API.Exists(name)
     local ctxs = GetAllContexts()
-    if not isstring(name) or TIsEmpty(ctxs) then return end
+    if not isstring(name) or tisempty(ctxs) then return end
 
     for _, v in pairs(ctxs) do
         if v:HasLogger(name) then return true end
