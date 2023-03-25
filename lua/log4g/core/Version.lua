97d6f7cbe58a98fc9da4570355acb29eb013f62b
graywolf64 <tairikuookami@foxmail.com>
2023-03-25 13:14:29 +0800

    Removed unused `_VERSION` field.

--- lua/log4g/core/Version.lua
+++ lua/log4g/core/Version.lua
@@ -1,7 +1,5 @@
 --- Version handler.
 -- @script Version
-Log4g._VERSION = Log4g._VERSION or {}
-
 concommand.Add("Log4g_Version", function()
     http.Fetch("https://raw.githubusercontent.com/GrayWolf64/gmod-logging-log4g/main/VERSION.txt", function(body)
         print(body)
