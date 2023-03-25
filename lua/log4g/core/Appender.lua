f6f8ddb6cd0f8e9b7bfa12d98a447e74b5623fcc
graywolf64 <tairikuookami@foxmail.com>
2023-03-25 10:25:40 +0800

    Appender use LifeCycle's ptable.

--- lua/log4g/core/Appender.lua
+++ lua/log4g/core/Appender.lua
@@ -5,19 +5,14 @@ Log4g.Core.Appender = Log4g.Core.Appender or {}
 local LifeCycle = Log4g.Core.LifeCycle.GetClass()
 local Appender = LifeCycle:subclass("Appender")
 
-local PRIVATE = PRIVATE or setmetatable({}, {
-    __mode = "k"
-})
-
 function Appender:Initialize(name, layout)
     LifeCycle.Initialize(self)
-    PRIVATE[self] = {}
-    PRIVATE[self].layout = layout
+    self:SetPrivateField("layout", layout)
     self.name = name
 end
 
 function Appender:GetLayout()
-    return PRIVATE[self].layout
+    return self:GetPrivateField("layout")
 end
 
 function Log4g.Core.Appender.GetClass()
