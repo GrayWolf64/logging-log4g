ce5f99cf6d2649f88faa5dbda6657d001d78336d
graywolf64 <tairikuookami@foxmail.com>
2023-04-01 00:41:45 +0800

    Removed unused funcs.

--- lua/log4g/mmc-gui/client/ClientGUIDerma.lua
+++ lua/log4g/mmc-gui/client/ClientGUIDerma.lua
@@ -62,45 +62,4 @@ function ClientGUIDerma.GetRowControl(row)
     return row:GetChild(1):GetChild(0):GetChild(0)
 end
 
---- Get a line's content text(s) at specific columns.
--- @param num The number of the line
--- @param listview The DListView containing the line
--- @param ... The texts of the specific column headers
--- @return tbl results
-function ClientGUIDerma.GetColumnSpecialText(num, listview, ...)
-    local line = listview:GetLine(num)
-    if not IsValid(line) then return end
-
-    local tbl, args = {}, {...}
-
-    for m, n in ipairs(listview.Columns) do
-        local text = n:GetChild(0):GetText()
-        local str = line:GetColumnText(m)
-
-        for _, v in pairs(args) do
-            if v == text then
-                tbl[text] = str
-            end
-        end
-    end
-
-    return tbl
-end
-
---- Set a DListView's line's text correctly using the given table with string keys and string values.
--- @param tbl The table containing the needed text values, and its keys must be the same with the column texts
--- @param line The line to set the texts in
--- @param panel The DListView
-function ClientGUIDerma.SetProperLineText(tbl, line, panel)
-    if table.IsEmpty(panel.Columns) then return end
-
-    for i, j in pairs(tbl) do
-        for m, n in ipairs(panel.Columns) do
-            if i == n:GetChild(0):GetText() then
-                line:SetColumnText(m, j)
-            end
-        end
-    end
-end
-
 return ClientGUIDerma
