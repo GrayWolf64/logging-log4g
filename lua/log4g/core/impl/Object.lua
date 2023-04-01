61f6a30d6344f7b2604419406438a2030fcec404
graywolf64 <tairikuookami@foxmail.com>
2023-04-01 11:03:27 +0800

    Added some comments.

--- lua/log4g/core/impl/Object.lua
+++ lua/log4g/core/impl/Object.lua
@@ -1,3 +1,7 @@
+--- Class Object is the root of the class hierarchy.
+-- @classmod Object
+-- @license Apache License 2.0
+-- @copyright GrayWolf64
 local Object = include("log4g/core/impl/MiddleClass.lua")("Object")
 
 function Object:IsObject()
