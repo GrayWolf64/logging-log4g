36908c18825d7a6c3f1c39fb9b78580fe8a4b540
graywolf64 <tairikuookami@foxmail.com>
2023-04-01 12:59:21 +0800

    Object GetName and SetName.

--- lua/log4g/core/impl/Object.lua
+++ lua/log4g/core/impl/Object.lua
@@ -4,7 +4,7 @@
 -- @copyright GrayWolf64
 Log4g.Core.Object = Log4g.Core.Object or {}
 local SHA256 = util.SHA256
-local tostring = tostring
+local tostring, isstring = tostring, isstring
 local Object = include("log4g/core/impl/MiddleClass.lua")("Object")
 
 local PRIVATE = PRIVATE or setmetatable({}, {
@@ -17,6 +17,15 @@ end
 
 function Object:IsObject()
     return true
+end
+
+function Object:SetName(name)
+    if not isstring(name) then return end
+    PRIVATE[self].name = name
+end
+
+function Object:GetName()
+    return PRIVATE[self].name
 end
 
 function Object:SetPrivateField(key, value)
