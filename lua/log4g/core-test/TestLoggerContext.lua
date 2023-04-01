10b7b69698978e145d2d1dbb81fc6c43915f5aa8
graywolf64 <tairikuookami@foxmail.com>
2023-04-01 12:42:07 +0800

    Changed TestLoggerContext.

--- lua/log4g/core-test/TestLoggerContext.lua
+++ lua/log4g/core-test/TestLoggerContext.lua
@@ -2,15 +2,13 @@ local Get = Log4g.Core.LoggerContext.Get
 local GetContext = Log4g.API.LoggerContextFactory.GetContext
 
 concommand.Add("Log4g_CoreTest_CreateLoggerContext", function()
-    GetContext("Foo")
-    GetContext("Bar", false)
-    print("is 'Foo' Configuration created: ", Get("Foo"):GetConfiguration() ~= nil)
-    print("is 'Bar' Configuration created: ", Get("Bar"):GetConfiguration() ~= nil)
+    GetContext("X")
+    GetContext("Y", false)
 end)
 
 concommand.Add("Log4g_CoreTest_RemoveLoggerContext", function()
-    Get("Foo"):Terminate()
-    Get("Bar"):Terminate()
+    Get("X"):Terminate()
+    Get("Y"):Terminate()
 end)
 
 concommand.Add("Log4g_CoreTest_ShowAllLoggerContext", function()
