561fcc406f9a882c5bd12215980ab854ce97efb3
graywolf64 <tairikuookami@foxmail.com>
2023-04-02 12:38:40 +0800

    Added one convar.

--- lua/log4g/core/layout/PatternLayout.lua
+++ lua/log4g/core/layout/PatternLayout.lua
@@ -3,6 +3,7 @@
 Log4g.Core.Layout.PatternLayout = Log4g.Core.Layout.PatternLayout or {}
 local Layout = Log4g.Core.Layout.GetClass()
 local PatternLayout = Layout:subclass("PatternLayout")
+CreateConVar("log4g.patternlayout.ConversionPattern", "%-5p [%t]: %m%n", FCVAR_NOTIFY)
 
 function PatternLayout:Initialize(name)
     Layout.Initialize(self, name)
