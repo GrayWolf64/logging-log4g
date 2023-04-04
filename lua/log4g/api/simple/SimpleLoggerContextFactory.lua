5029129a964ee3586e69e69a9d61ed98fff1fc33
graywolf64 <tairikuookami@foxmail.com>
2023-04-04 22:42:13 +0800

    Fixed load order.

--- lua/log4g/api/simple/SimpleLoggerContextFactory.lua
+++ lua/log4g/api/simple/SimpleLoggerContextFactory.lua
@@ -1,3 +1,4 @@
+Log4g.API.SimpleLoggerContextFactory = Log4g.API.SimpleLoggerContextFactory or {}
 local RegisterSimpleLoggerContext = Log4g.API.Simple.SimpleLoggerContext.Register
 
 function Log4g.API.SimpleLoggerContextFactory.GetContext(name)
