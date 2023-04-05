c7d9e9873488691b28df5255bbc0f0ef8e33da56
graywolf64 <tairikuookami@foxmail.com>
2023-04-05 13:26:12 +0800

    Make CoreTest a concommand.

--- lua/log4g/core-test/CoreTest.lua
+++ lua/log4g/core-test/CoreTest.lua
@@ -1,2 +1,4 @@
-include("log4g/core-test/TestLoggerContext.lua")
-include("log4g/core-test/TestLogger.lua")
+concommand.Add("Log4g_LoadCoreTest", function()
+    include("log4g/core-test/TestLoggerContext.lua")
+    include("log4g/core-test/TestLogger.lua")
+end)
