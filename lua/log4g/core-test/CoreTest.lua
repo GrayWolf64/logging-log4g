6c50206fab69db9e83d0107bc709057cb4a53046
graywolf64 <tairikuookami@foxmail.com>
2023-04-05 18:29:57 +0800

    Fixed typo.

--- lua/log4g/core-test/CoreTest.lua
+++ lua/log4g/core-test/CoreTest.lua
@@ -1,4 +1,4 @@
-concommand.Add("Log4g_load_coretest", function()
+concommand.Add("log4g_load_coretest", function()
     include("log4g/core-test/TestLoggerContext.lua")
     include("log4g/core-test/TestLogger.lua")
 end)
