811f2b2c23327a9cfecc3ee015390478aab00af5
graywolf64 <tairikuookami@foxmail.com>
2023-04-08 09:22:56 +0800

    Rework DoFormat.

--- lua/log4g/core/layout/PatternLayout.lua
+++ lua/log4g/core/layout/PatternLayout.lua
@@ -5,7 +5,6 @@ local Layout = Log4g.Core.Layout.GetClass()
 local PatternLayout = Layout:subclass("PatternLayout")
 local IsLogEvent = include("log4g/core/util/TypeUtil.lua").IsLogEvent
 local cvar_cp = "log4g.patternlayout.ConversionPattern"
-local debug_getinfo = debug.getinfo
 CreateConVar(cvar_cp, "%d %p [%t]: %m%n", FCVAR_NOTIFY)
 
 function PatternLayout:Initialize(name)
@@ -13,21 +12,7 @@ function PatternLayout:Initialize(name)
 end
 
 local function DoFormat(event)
-    local res = GetConVar(cvar_cp):GetString()
-
-    local rep = {
-        ["%%d"] = event:GetTime(),
-        ["%%m"] = event:GetMsg(),
-        ["%%n"] = "\n",
-        ["%%t"] = debug_getinfo(6, "S").source:GetFileFromFilename(),
-        ["%%p"] = event:GetLevel():GetName()
-    }
-
-    for k, v in pairs(rep) do
-        res = res:gsub(k, v)
-    end
-
-    return res
+    GetConVar(cvar_cp):GetString()
 end
 
 function PatternLayout:Format(event)
