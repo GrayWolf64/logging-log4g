4ca8b0ee7371905f90efc026b5f8d912b0a0edba
graywolf64 <tairikuookami@foxmail.com>
2023-04-08 22:01:55 +0800

    Some minor changes.

--- lua/log4g/core/layout/PatternLayout.lua
+++ lua/log4g/core/layout/PatternLayout.lua
@@ -31,11 +31,11 @@ local function DoFormat(event)
         end
     end
 
-    local keys = {"%msg", "%endl", "%uptime", "%file", "%level"}
+    local tokens = {"%msg", "%endl", "%uptime", "%file", "%level"}
 
-    for _, v in pairs(keys) do
+    for _, v in pairs(tokens) do
         for i, j in ipairs(substrs) do
-            if j:find("%" .. v) then
+            if j:find(v, 1, true) then
                 local oldvalue = substrs[i]
                 table_remove(substrs, i)
                 table_insert(substrs, i, v)
@@ -44,6 +44,12 @@ local function DoFormat(event)
         end
     end
 
+    --- Make a function that can replace a table's matching values with replacement content(string),
+    -- and insert a Color before each replaced value.
+    -- @lfunction mkfunc_precolor
+    -- @param token String to search for and to be replaced
+    -- @param color Color object to insert before each replaced value
+    -- @return function output func
     local function mkfunc_precolor(token, color)
         return function(tbl, content)
             local i = {}
@@ -65,11 +71,11 @@ local function DoFormat(event)
     end
 
     local lv = event:GetLevel()
-    mkfunc_precolor("%level", lv:GetColor())(substrs, lv:GetName())
-    mkfunc_precolor("%msg", color_white)(substrs, event:GetMsg())
-    mkfunc_precolor("%file", color_white)(substrs, event:GetSource():GetFileFromFilename())
-    mkfunc_precolor("%endl")(substrs, "\n")
-    mkfunc_precolor("%uptime")(substrs, event:GetTime())
+    mkfunc_precolor(tokens[5], lv:GetColor())(substrs, lv:GetName())
+    mkfunc_precolor(tokens[1], color_white)(substrs, event:GetMsg())
+    mkfunc_precolor(tokens[4], color_white)(substrs, event:GetSource():GetFileFromFilename())
+    mkfunc_precolor(tokens[2])(substrs, "\n")
+    mkfunc_precolor(tokens[3])(substrs, event:GetTime())
 
     return unpack(substrs)
 end
