0be7fc8b1bbf6551ebf07d17d76254fcaadd712b
graywolf64 <tairikuookami@foxmail.com>
2023-04-08 16:37:40 +0800

    Completely reworked `DoFormat()` and fixed it. There may be some improvements to be done too.

--- lua/log4g/core/layout/PatternLayout.lua
+++ lua/log4g/core/layout/PatternLayout.lua
@@ -5,14 +5,74 @@ local Layout = Log4g.Core.Layout.GetClass()
 local PatternLayout = Layout:subclass("PatternLayout")
 local IsLogEvent = include("log4g/core/util/TypeUtil.lua").IsLogEvent
 local cvar_cp = "log4g.patternlayout.ConversionPattern"
-CreateConVar(cvar_cp, "%d %p [%t]: %m%n", FCVAR_NOTIFY)
+local debug_getinfo = debug.getinfo
+local pairs, ipairs = pairs, ipairs
+local unpack = unpack
+local table_insert, table_remove = table.insert, table.remove
+local CharPos = include("log4g/core/util/StringUtil.lua").CharPos
+CreateConVar(cvar_cp, "%uptime %level [%file]: %msg%endl", FCVAR_NOTIFY)
 
 function PatternLayout:Initialize(name)
     Layout.Initialize(self, name)
 end
 
 local function DoFormat(event)
-    GetConVar(cvar_cp):GetString()
+    local cp = GetConVar(cvar_cp):GetString()
+    local pos = CharPos(cp, "%")
+    if pos == true then return cp end
+    local substrs = {}
+
+    for k, v in ipairs(pos) do
+        local n = pos[k + 1]
+
+        if n then
+            substrs[k] = cp:sub(v, n - 1)
+        else
+            substrs[k] = cp:sub(v, #cp)
+        end
+    end
+
+    local keys = {"%msg", "%endl", "%uptime", "%file", "%level"}
+
+    for _, v in pairs(keys) do
+        for i, j in ipairs(substrs) do
+            if j:find("%" .. v) then
+                local oldvalue = substrs[i]
+                table_remove(substrs, i)
+                table_insert(substrs, i, v)
+                table_insert(substrs, i + 1, oldvalue:sub(#v + 1, #oldvalue))
+            end
+        end
+    end
+
+    local function mkfunc_precolor(token, color)
+        return function(tbl, content)
+            local i = {}
+
+            for k, v in ipairs(tbl) do
+                if v == token then
+                    i[k] = true
+                end
+            end
+
+            for idx in pairs(i) do
+                tbl[idx] = content
+
+                if color then
+                    table_insert(tbl, idx - 1, color)
+                end
+            end
+        end
+    end
+
+    local lv = event:GetLevel()
+    mkfunc_precolor("%level", lv:GetColor())(substrs, lv:GetName())
+    mkfunc_precolor("%msg", color_white)(substrs, event:GetMsg())
+    mkfunc_precolor("%file", color_white)(substrs, debug_getinfo(6, "S").source:GetFileFromFilename())
+    mkfunc_precolor("%endl")(substrs, "\n")
+    mkfunc_precolor("%uptime")(substrs, event:GetTime())
+
+    return unpack(substrs)
 end
 
 function PatternLayout:Format(event)
--- lua/log4g/core/util/StringUtil.lua
+++ lua/log4g/core/util/StringUtil.lua
@@ -4,7 +4,7 @@
 -- @copyright GrayWolf64
 local StringUtil = {}
 local string_explode = string.Explode
-local table_concat = table.concat
+local table_concat, table_insert = table.concat, table.insert
 local isstring = isstring
 
 --- Qualifies the string name of an object and returns if it's a valid name.
@@ -36,6 +36,24 @@ function StringUtil.StripDotExtension(str, doconcat)
     else
         return result
     end
+end
+
+--- Get all the positions of a char in a string.
+-- @param str String to search in
+-- @param char A Single character to search for
+-- @return table positions or true if not found
+function StringUtil.CharPos(str, char)
+    if not isstring(str) or not isstring(char) or not #char == 1 then return end
+    local pos = {}
+    char = char:byte()
+
+    for k, v in ipairs({str:byte(1, #str)}) do
+        if v == char then
+            table_insert(pos, k)
+        end
+    end
+
+    return not #pos or pos
 end
 
 return StringUtil
