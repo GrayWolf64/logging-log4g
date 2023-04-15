fbec54095b990237ac0e9ce8fe76e391e4f15f44
graywolf64 <tairikuookami@foxmail.com>
2023-04-15 17:24:02 +0800

    Fixed TypeUtil.

--- lua/log4g/core/util/TypeUtil.lua
+++ lua/log4g/core/util/TypeUtil.lua
@@ -1,20 +1,40 @@
 local TypeUtil = {}
-local type, assert = type, assert
-local tostring = tostring
+local pairs = pairs
 
-local classes = {"Object", "LoggerContext", "Configuration", "Level", "Appender", "Layout", "Logger", "LogEvent", "LoggerConfig", "RootLoggerConfig"}
+local classes = {
+    ["Object"] = {"LifeCycle", "RootLoggerConfig", "LoggerConfig", "LogEvent", "LoggerContext", "Configuration", "Level", "Layout", "Logger", "Appender", "DefaultConfiguration"},
+    ["Configuration"] = {"DefaultConfiguration"},
+    ["LoggerConfig"] = {"RootLoggerConfig"},
+    ["Appender"] = {"ConsoleAppender"},
+    ["Layout"] = {"PatternLayout"},
+    ["LoggerContext"] = {},
+    ["Level"] = {},
+    ["Logger"] = {},
+    ["LogEvent"] = {},
+    ["RootLoggerConfig"] = {}
+}
 
-local function mkfunc_classcheck(cls)
+local function mkfunc_classcheck(cls, subclss)
     return function(o)
+        if not o then return false end
         local class = o.class
-        assert(class, "MiddleClass object expected, got " .. type(o) .. ".")
+        if not class then return false end
         local clsname = class.name
-        assert(clsname == cls, "'" .. cls .. "' object expected, got " .. tostring(clsname) .. ".")
+
+        if subclss then
+            for _, v in pairs(subclss) do
+                if v == clsname then return true end
+            end
+        end
+
+        if clsname == cls then return true end
+
+        return false
     end
 end
 
-for k in pairs(classes) do
-    TypeUtil["Is" .. k] = mkfunc_classcheck(k)
+for k, v in pairs(classes) do
+    TypeUtil["Is" .. k] = mkfunc_classcheck(k, v)
 end
 
 return TypeUtil
