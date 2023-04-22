230391c4834aabc522796d4d124d9b316e746a99
graywolf64 <tairikuookami@foxmail.com>
2023-04-22 19:16:39 +0800

    Updated Core version.

--- lua/log4g/core/Core.lua
+++ lua/log4g/core/Core.lua
@@ -2,7 +2,7 @@
 -- @script Core
 -- @license Apache License 2.0
 -- @copyright GrayWolf64
-Log4g.RegisterPackage("log4g-core", "0.0.5-beta")
+Log4g.RegisterPackage("log4g-core", "0.0.6-beta")
 Log4g.Core = Log4g.Core or {}
 Log4g.Core.Config = Log4g.Core.Config or {}
 include("log4g/core/Version.lua")
