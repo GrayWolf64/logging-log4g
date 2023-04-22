f879525b163c7994c7bb9efdb37f1e901894923e
graywolf64 <tairikuookami@foxmail.com>
2023-04-22 19:53:44 +0800

    Updated Version.lua.

--- lua/log4g/core/Version.lua
+++ lua/log4g/core/Version.lua
@@ -1,9 +1,29 @@
 --- Version handler.
 -- @script Version
+local MsgN, pairs = MsgN, pairs
+
+local function getVersionString()
+    local verstr = ""
+
+    for k, v in pairs(Log4g.GetAllPackages()) do
+        if #verstr == 0 then
+            verstr = k .. " " .. v.version .. ","
+        else
+            verstr = verstr .. " " .. k .. " " .. v.version .. ","
+        end
+    end
+
+    return verstr
+end
+
 concommand.Add("log4g_version", function()
+    MsgN("Local version:")
+    MsgN(getVersionString())
+
     http.Fetch("https://raw.githubusercontent.com/GrayWolf64/gmod-logging-log4g/main/VERSION.txt", function(body)
-        print(body)
+        MsgN("Github latest 'VERSION.txt':")
+        MsgN(body)
     end, function(msg)
-        print(msg)
+        MsgN(msg)
     end)
 end)
