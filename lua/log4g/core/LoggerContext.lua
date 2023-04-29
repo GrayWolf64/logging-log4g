a950be814e13a347e0a70fc2d9014f8258342551
graywolf64 <tairikuookami@foxmail.com>
2023-04-29 23:56:35 +0800

    LoggerContext default source will be 'unknown'.

--- lua/log4g/core/LoggerContext.lua
+++ lua/log4g/core/LoggerContext.lua
@@ -17,6 +17,7 @@ local type = type
 function LoggerContext:Initialize(name)
     LifeCycle.Initialize(self)
     self:SetPrivateField("logger", {})
+    self:SetPrivateField("source", "unknown")
     self:SetName(name)
 end
 
