a338de404b1a202634c5d58076d7148bafbece3c
graywolf64 <tairikuookami@foxmail.com>
2023-04-30 00:00:45 +0800

    Removed brackets in MMC init.

--- lua/log4g/mmc-gui/MMC.lua
+++ lua/log4g/mmc-gui/MMC.lua
@@ -5,9 +5,9 @@
 -- @copyright GrayWolf64
 if SERVER then
     Log4g.RegisterPackage("log4g-mmc", "0.0.5-beta")
-    include("log4g/mmc-gui/server/ClientGUI.lua")
-    AddCSLuaFile("log4g/mmc-gui/client/MMCDerma.lua")
-    AddCSLuaFile("log4g/mmc-gui/client/ClientGUI.lua")
+    include"log4g/mmc-gui/server/ClientGUI.lua"
+    AddCSLuaFile"log4g/mmc-gui/client/MMCDerma.lua"
+    AddCSLuaFile"log4g/mmc-gui/client/ClientGUI.lua"
 elseif CLIENT then
-    include("log4g/mmc-gui/client/ClientGUI.lua")
+    include"log4g/mmc-gui/client/ClientGUI.lua"
 end
