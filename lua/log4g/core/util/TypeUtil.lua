893891215b0d4b95e871b7448fd2d53101dd829a
graywolf64 <tairikuookami@foxmail.com>
2023-04-29 19:42:48 +0800

    Some prints.

--- lua/log4g/core/util/TypeUtil.lua
+++ lua/log4g/core/util/TypeUtil.lua
@@ -5,6 +5,7 @@
 local TypeUtil = {}
 local pairs = pairs
 local istable = istable
+local print = print
 
 --- All the `Class` names in Log4g.
 -- @local
@@ -44,11 +45,26 @@ local Classes = {
     ["LoggerConfig.RootLogger"] = {}
 }
 
+function print(...)
+    print("Log4g-userinput:", ...)
+end
+
 local function mkfunc_classcheck(cls, subclss)
     return function(o)
-        if not o or not istable(o) then return false end
+        if not o or not istable(o) then
+            print("expecting table, got", type(o), ".")
+
+            return false
+        end
+
         local class = o.class
-        if not class then return false end
+
+        if not class then
+            print("expecting MiddleClass object, got a normal table.")
+
+            return false
+        end
+
         local clsname = class.name
 
         if subclss then
@@ -58,6 +74,7 @@ local function mkfunc_classcheck(cls, subclss)
         end
 
         if clsname == cls then return true end
+        print(clsname, "is not the expected class or is not its subclass.")
 
         return false
     end
