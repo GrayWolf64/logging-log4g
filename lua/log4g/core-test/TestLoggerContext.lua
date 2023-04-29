c775b1ac8356b75117d178c1633c17de832abac4
graywolf64 <tairikuookami@foxmail.com>
2023-04-29 20:21:41 +0800

    Updated LoggerContext test.

--- lua/log4g/core-test/TestLoggerContext.lua
+++ lua/log4g/core-test/TestLoggerContext.lua
@@ -1,15 +1,24 @@
 local LoggerContext = Log4g.GetPkgClsFuncs("log4g-core", "LoggerContext")
 local Get = LoggerContext.get
 local GetContext = Log4g.API.LoggerContextFactory.GetContext
+local randomString = Log4g.CoreTest.randomString
+local print = print
+local names = {}
 
 concommand.Add("log4g_coretest_createLoggerContext", function()
-    GetContext("X")
-    GetContext("Y", false)
-end)
+    print("starting LoggerContext create unittest:")
 
-concommand.Add("log4g_coretest_removeLoggerContext", function()
-    Get("X"):Terminate()
-    Get("Y"):Terminate()
+    for i = 1, 10 do
+        local name = randomString(10)
+        names[i] = name
+        GetContext(name)
+        print("i = ", i, "created", name)
+    end
+
+    for _, v in pairs(names) do
+        Get(v):Terminate()
+        print("terminated", v)
+    end
 end)
 
 concommand.Add("log4g_coretest_showAllLoggerContext", function()
