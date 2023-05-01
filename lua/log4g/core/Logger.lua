bf0d8077c0fa834fff58c18c41307837c1680d72
graywolf64 <tairikuookami@foxmail.com>
2023-05-01 16:14:08 +0800

    Merged LoggerConfig and Logger.

--- lua/log4g/core/Logger.lua
+++ lua/log4g/core/Logger.lua
@@ -2,20 +2,223 @@
 -- @classmod Logger
 -- @license Apache License 2.0
 -- @copyright GrayWolf64
-local Object = Log4g.GetPkgClsFuncs("log4g-core", "Object").getClass()
-local Logger = Object:subclass("Logger")
-local GetCtx = Log4g.GetPkgClsFuncs("log4g-core", "LoggerContext").get
-local HasLoggerConfig = Log4g.GetPkgClsFuncs("log4g-core", "LoggerConfig").hasLoggerConfig
-local EnumerateAncestors = Log4g.GetPkgClsFuncs("log4g-core", "Object").enumerateAncestors
+local Object = Log4g.GetPkgClsFuncs("log4g-core", "Object")
+local LoggerContext = Log4g.GetPkgClsFuncs("log4g-core", "LoggerContext")
 local GetLevel = Log4g.GetPkgClsFuncs("log4g-core", "Level").getLevel
-local TypeUtil, StringUtil = Log4g.GetPkgClsFuncs("log4g-core", "TypeUtil"), include("log4g/core/util/StringUtil.lua")
+local TypeUtil = Log4g.GetPkgClsFuncs("log4g-core", "TypeUtil")
+local StringUtil = include("log4g/core/util/StringUtil.lua")
 local PropertiesPlugin = Log4g.GetPkgClsFuncs("log4g-core", "PropertiesPlugin")
+local LifeCycle = Log4g.GetPkgClsFuncs("log4g-core", "LifeCycle").getClass()
+local GetCtx, GetAllCtx = LoggerContext.get, LoggerContext.getAll
+local EnumerateAncestors = Object.enumerateAncestors
 local QualifyName, StripDotExtension = StringUtil.QualifyName, StringUtil.StripDotExtension
 local IsLoggerConfig, IsLoggerContext = TypeUtil.IsLoggerConfig, TypeUtil.IsLoggerContext
+local IsAppender, IsConfiguration = TypeUtil.IsAppender, TypeUtil.IsConfiguration
 local IsLevel, IsLogEvent = TypeUtil.IsLevel, TypeUtil.IsLogEvent
 TypeUtil, StringUtil = nil, nil
-local next, pairs = next, pairs
-local type = type
+local tableConcat = table.concat
+local next, pairs, type = next, pairs, type
+PropertiesPlugin.registerProperty("rootLoggerName", "root", true)
+--- Logger object that is created via configuration.
+-- @type LoggerConfig
+local LoggerConfig = LifeCycle:subclass"LoggerConfig"
+LoggerConfig:include(Object.contextualMixins)
+
+function LoggerConfig:Initialize(name)
+    LifeCycle.Initialize(self)
+    self:SetPrivateField("apref", {})
+    self:SetName(name)
+end
+
+function LoggerConfig:__tostring()
+    return "LoggerConfig: [name:" .. self:GetName() .. "]"
+end
+
+--- Sets the log Level.
+-- @param level The Logging Level
+function LoggerConfig:SetLevel(level)
+    if not IsLevel(level) then return end
+    if self:GetPrivateField"level" == level then return end
+    self:SetPrivateField("level", level)
+end
+
+function LoggerConfig:GetLevel()
+    return self:GetPrivateField"level"
+end
+
+local function HasLoggerConfig(name, context)
+    local getlc = function(ctx, lcn) return ctx:GetConfiguration():GetLoggerConfig(lcn) end
+
+    if not IsLoggerContext(context) then
+        for _, v in pairs(GetAllCtx()) do
+            if getlc(v, name) then return true end
+        end
+    else
+        if getlc(context, name) then return true end
+    end
+
+    return false
+end
+
+local function GetLoggerConfig(name)
+    for _, v in pairs(GetAllCtx()) do
+        local loggerConfig = v:GetConfiguration():GetLoggerConfig(name)
+        if loggerConfig then return loggerConfig end
+    end
+end
+
+--- Sets the parent of this LoggerConfig.
+-- @param T LoggerConfig object or LoggerConfig name
+function LoggerConfig:SetParent(T)
+    if type(T) == "string" then
+        if not HasLoggerConfig(T, GetCtx(self:GetContext())) then return end
+        self:SetPrivateField("parent", T)
+    elseif IsLoggerConfig(T) and T:GetContext() == self:GetContext() then
+        self:SetPrivateField("parent", T:GetName())
+    end
+end
+
+--- Gets the parent of this LoggerConfig.
+-- @return string lcname
+function LoggerConfig:GetParent()
+    return self:GetPrivateField"parent"
+end
+
+function LoggerConfig:GetAppenderRef()
+    return self:GetPrivateField"apref"
+end
+
+--- Adds an Appender to the LoggerConfig.
+-- It adds the Appender name to the LoggerConfig's private `apref` table field,
+-- then adds the Appender object to the Configuration's(the only one which owns this LoggerConfig) private `appender` table field.
+-- @param ap Appender object
+-- @return bool ifadded
+function LoggerConfig:AddAppender(ap)
+    if not IsAppender(ap) then return end
+    self:GetAppenderRef()[ap:GetName()] = true
+
+    return GetCtx(self:GetContext()):GetConfiguration():AddAppender(ap, self:GetName())
+end
+
+--- Returns all Appenders configured by this LoggerConfig in a form of table (keys are Appenders, values are booleans).
+-- @return table appenders
+function LoggerConfig:GetAppenders()
+    local appenders, config, apref = {}, GetCtx(self:GetContext()):GetConfiguration(), self:GetAppenderRef()
+    if not next(apref) then return end
+
+    for appenderName in pairs(apref) do
+        appenders[config:GetAppenders()[appenderName]] = true
+    end
+
+    return appenders
+end
+
+--- Removes all Appenders configured by this LoggerConfig.
+function LoggerConfig:ClearAppenders()
+    local config, apref = GetCtx(self:GetContext()):GetConfiguration(), self:GetAppenderRef()
+    if not next(apref) then return end
+
+    for appenderName in pairs(apref) do
+        config:RemoveAppender(appenderName)
+        self:GetAppenderRef()[appenderName] = nil
+    end
+end
+
+--- The root LoggerConfig.
+-- @type LoggerConfig.RootLogger
+local RootLoggerConfig = LoggerConfig:subclass"LoggerConfig.RootLogger"
+
+function RootLoggerConfig:Initialize()
+    LoggerConfig.Initialize(self, PropertiesPlugin.getProperty("rootLoggerName", true))
+    self:SetLevel(GetLevel"INFO")
+end
+
+function RootLoggerConfig:__tostring()
+    return "RootLoggerConfig: [name:" .. self:GetName() .. "]"
+end
+
+--- Overrides `LoggerConfig:SetParent()`.
+-- @return bool false
+function RootLoggerConfig:SetParent()
+    return false
+end
+
+--- Overrides `LoggerConfig:GetParent()`.
+-- @return bool false
+function RootLoggerConfig:GetParent()
+    return false
+end
+
+-- @section end
+local function GetRootLoggerConfigClass()
+    return RootLoggerConfig
+end
+
+--- Check if a LoggerConfig's ancestors exist and return its desired parent name.
+-- @lfunction ValidateAncestors
+-- @param loggerConfig LoggerConfig object
+-- @return bool valid
+-- @return string parent name
+local function ValidateAncestors(loggerConfig)
+    local ancestors, nodes = EnumerateAncestors(loggerConfig:GetName())
+
+    local function HasEveryLoggerConfig(tbl)
+        local ctx = GetCtx(loggerConfig:GetContext())
+
+        for k in pairs(tbl) do
+            if not HasLoggerConfig(k, ctx) then return false end
+        end
+
+        return true
+    end
+
+    if HasEveryLoggerConfig(ancestors) then return true, tableConcat(nodes, ".") end
+
+    return false
+end
+
+--- Factory method to create a LoggerConfig.
+-- @param name The name for the Logger
+-- @param config The Configuration
+-- @param level The Logging Level
+-- @return object loggerconfig
+local function CreateLoggerConfig(name, config, level)
+    local root = PropertiesPlugin.getProperty("rootLoggerName", true)
+    if not IsConfiguration(config) or name == root then return end
+    local loggerConfig = LoggerConfig(name)
+    loggerConfig:SetContext(config:GetContext())
+
+    local setLevelAndParent = function(o, level1, level2, parent)
+        if IsLevel(level1) then
+            o:SetLevel(level1)
+        else
+            o:SetLevel(level2)
+        end
+
+        o:SetParent(parent)
+    end
+
+    if name:find"%." then
+        local valid, parent = ValidateAncestors(loggerConfig)
+        if not valid then return end
+        setLevelAndParent(loggerConfig, level, GetLoggerConfig(parent):GetLevel(), parent)
+    else
+        setLevelAndParent(loggerConfig, level, config:GetRootLogger():GetLevel(), root)
+    end
+
+    config:AddLogger(name, loggerConfig)
+
+    return loggerConfig
+end
+
+Log4g.RegisterPackageClass("log4g-core", "LoggerConfig", {
+    create = CreateLoggerConfig,
+    getRootLoggerConfigClass = GetRootLoggerConfigClass,
+    hasLoggerConfig = HasLoggerConfig
+})
+
+Object = Object.getClass()
+local Logger = Object:subclass("Logger")
 
 function Logger:Initialize(name, context)
     Object.Initialize(self)
@@ -218,7 +421,7 @@ function Logger:Fatal(msg)
     LogIfEnabled(self, "FATAL", msg)
 end
 
-local function Create(loggerName, context, loggerconfig)
+local function CreateLogger(loggerName, context, loggerconfig)
     if not IsLoggerContext(context) then return end
     if context:HasLogger(loggerName) or not QualifyName(loggerName) then return end
     local logger, root = Logger(loggerName, context), PropertiesPlugin.getProperty("rootLoggerName", true)
@@ -272,5 +475,5 @@ end
 
 Log4g.RegisterPackageClass("log4g-core", "Logger", {
     getClass = GetClass,
-    create = Create
+    create = CreateLogger
 })
