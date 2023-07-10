29e8473412fa9e1ac1883fd3230da1c123bc709c
GrayWolf <tairikuookami@foxmail.com>
2023-07-10 21:20:37 +0800

    Some changes on CoreTest.

--- lua/log4g/core-test/CoreTest.lua
+++ lua/log4g/core-test/CoreTest.lua
@@ -7,15 +7,16 @@ concommand.Add("log4g_load_coretest", function()
     Log4g.CoreTest = Log4g.CoreTest or {}
 
     function Log4g.CoreTest.randomString(len)
-        local res = ""
+        local chars = {}
 
         for i = 1, len do
-            res = res .. string.char(math.random(97, 122))
+            table.insert(chars, string.char(math.random(97, 122)))
         end
 
-        return res
+        return table.concat(chars)
     end
 
+    include"log4g/core-test/ExtendedLevels.lua"
     include"log4g/core-test/TestLoggerContext.lua"
     include"log4g/core-test/TestLogger.lua"
 end)
---
+++ lua/log4g/core-test/ExtendedLevels.lua
@@ -0,0 +1,3 @@
+local NOTE = Log4g.Core.Level.forName("NOTE", 350)
+NOTE:SetColor(Color(160, 185, 242))
+print(NOTE:GetName(), NOTE:GetColor())
