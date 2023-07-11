cad4550967a449865593011acbf506f7735abbea
GrayWolf <tairikuookami@foxmail.com>
2023-07-11 11:50:03 +0800

    Removed unused func.

--- lua/log4g/core-test/CoreTest.lua
+++ lua/log4g/core-test/CoreTest.lua
@@ -6,16 +6,6 @@
 concommand.Add("log4g_load_coretest", function()
     Log4g.CoreTest = Log4g.CoreTest or {}
 
-    function Log4g.CoreTest.randomString(len)
-        local chars = {}
-
-        for i = 1, len do
-            table.insert(chars, string.char(math.random(97, 122)))
-        end
-
-        return table.concat(chars)
-    end
-
     include"log4g/core-test/ExtendedLevels.lua"
     include"log4g/core-test/CoreLoggerContexts.lua"
     include"log4g/core-test/TestLogger.lua"
