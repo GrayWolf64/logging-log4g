313ddd8ce548254b61080b1f3060cb0dffefd477
GrayWolf <tairikuookami@foxmail.com>
2023-07-12 09:17:57 +0800

    Use MiddleClass's original asserts.

--- lua/log4g/core/impl/MiddleClass.lua
+++ lua/log4g/core/impl/MiddleClass.lua
@@ -101,7 +101,7 @@ local function _createClass(name, super)
 end
 
 local function _includeMixin(aClass, mixin)
-    if type(mixin) ~= "table" then return end
+    assert(type(mixin) == "table", "mixin must be a table")
 
     for name, method in pairs(mixin) do
         if name ~= "included" and name ~= "static" then
@@ -126,21 +126,22 @@ local DefaultMixin = {
     isInstanceOf = function(self, aClass) return type(aClass) == "table" and type(self) == "table" and (self.class == aClass or type(self.class) == "table" and type(self.class.isSubclassOf) == "function" and self.class:isSubclassOf(aClass)) end,
     static = {
         allocate = function(self)
-            if type(self) ~= "table" then return end
-
+            assert(type(self) == "table", "Make sure that you are using 'Class:allocate' instead of 'Class.allocate'")
             return setmetatable({
                 class = self,
             }, self.__instanceDict)
         end,
         New = function(self, ...)
-            if type(self) ~= "table" then return end
+            assert(type(self) == "table", "Make sure that you are using 'Class:new' instead of 'Class.new'")
             local instance = self:allocate()
             instance:Initialize(...)
 
             return instance
         end,
         subclass = function(self, name)
-            if type(self) ~= "table" or type(name) ~= "string" then return end
+            assert(type(self) == "table", "Make sure that you are using 'Class:subclass' instead of 'Class.subclass'")
+            assert(type(name) == "string", "You must provide a name(string) for your class")
+
             local subclass = _createClass(name, self)
 
             for methodName, f in pairs(self.__instanceDict) do
@@ -158,7 +159,7 @@ local DefaultMixin = {
         subclassed = function(self, other) end,
         isSubclassOf = function(self, other) return type(other) == "table" and type(self.super) == "table" and (self.super == other or self.super:isSubclassOf(other)) end,
         include = function(self, ...)
-            if type(self) ~= "table" then return end
+            assert(type(self) == "table", "Make sure you that you are using 'Class:include' instead of 'Class.include'")
 
             for _, mixin in ipairs({...}) do
                 _includeMixin(self, mixin)
@@ -170,7 +171,7 @@ local DefaultMixin = {
 }
 
 function MiddleClass.class(name, super)
-    if type(name) ~= "string" then return end
+    assert(type(name) == "string", "A name (string) is needed for the new class")
 
     return super and super:subclass(name) or _includeMixin(_createClass(name), DefaultMixin)
 end
